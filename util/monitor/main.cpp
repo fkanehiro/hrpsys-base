f50037c9f7ae980b06586b7ff2baa29c1c0cd788
Fumio KANEHIRO <fkanehiro@gmail.com>
2012-08-29 00:47:41 +0000

    updates usage
    git-svn-id: https://hrpsys-base.googlecode.com/svn/trunk@481 a991ac11-fb38-5095-8c12-a1ddb0715245

--- util/monitor/main.cpp
+++ util/monitor/main.cpp
@@ -44,7 +44,7 @@ hrp::BodyPtr createBody(const std::string& name, const ModelItem& mitem,
 int main(int argc, char* argv[]) 
 {
     if (argc < 2){
-        std::cerr << "Usage:" << argv[0] << " project.xml" << std::endl;
+        std::cerr << "Usage:" << argv[0] << " project.xml [-rh RobotHardwareComponent] [-sh StateHolder component] [-size size]" << std::endl;
         return 1;
     }
 
