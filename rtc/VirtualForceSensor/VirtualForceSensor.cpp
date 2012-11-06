3711e78d57d4077cf4936368ae024993a0e55bf6
Fumio KANEHIRO <fkanehiro@gmail.com>
2012-11-06 12:16:31 +0000

    filters torques(by ik0313)
    git-svn-id: https://hrpsys-base.googlecode.com/svn/tags/3.1.3@564 a991ac11-fb38-5095-8c12-a1ddb0715245

--- rtc/VirtualForceSensor/VirtualForceSensor.cpp
+++ rtc/VirtualForceSensor/VirtualForceSensor.cpp
@@ -37,7 +37,8 @@ VirtualForceSensor::VirtualForceSensor(RTC::Manager* manager)
     // <rtc-template block="initializer">
     m_qRefIn("qRef", m_qRef),
     m_qCurrentIn("qCurrent", m_qCurrent),
-    m_tauIn("tau", m_tau),
+    m_tauInIn("tauIn", m_tauIn),
+    m_tauOutOut("tauOut", m_tauOut),
     //m_VirtualForceSensorServicePort("VirtualForceSensorService"),
     // </rtc-template>
     m_debugLevel(0),
@@ -65,9 +66,10 @@ RTC::ReturnCode_t VirtualForceSensor::onInitialize()
   // Set InPort buffers
   addInPort("qRef", m_qRefIn);
   addInPort("qCurrent", m_qCurrentIn);
-  addInPort("tau", m_tauIn);
+  addInPort("tauIn", m_tauInIn);
 
   // Set OutPort buffer
+  addOutPort("tauOut", m_tauOutOut);
   
   // Set service provider to Ports
   //m_VirtualForceSensorServicePort.registerProvider("service0", "VirtualForceSensorService", m_VirtualForceSensorService);
@@ -98,6 +100,8 @@ RTC::ReturnCode_t VirtualForceSensor::onInitialize()
       std::cerr << "failed to load model[" << prop["model"] << "]"
 		<< std::endl;
   }
+
+  m_tauOut.data.length(m_robot->numJoints());
 
   // virtual_force_sensor: <name>, <base>, <target>, 0, 0, 0,  0, 0, 0, 1
   coil::vstring virtual_force_sensor = coil::split(prop["virtual_force_sensor"], ",");
@@ -151,6 +155,42 @@ RTC::ReturnCode_t VirtualForceSensor::onInitialize()
     std::cerr << " dead_zone " << m_error_dead_zone[i] << std::endl;
   }
 
+  // set torque offset
+  // offset = -(gear torque in neutral pose)
+  m_torque_offset.resize(m_robot->numJoints());
+  coil::vstring torque_offset = coil::split(prop["torque_offset"], ",");
+  assert(m_torque_offset.size() == torque_offset.size());
+  for(int i = 0; i < m_robot->numJoints(); i++){
+    coil::stringTo(m_torque_offset[i], torque_offset[i].c_str());
+    std::cerr << "offset[" << m_robot->joint(i)->name << "]: " << m_torque_offset[i] << std::endl;
+  }
+  
+  // make filter
+  // filter_dim, fb_coeffs[0], ..., fb_coeffs[filter_dim], ff_coeffs[0], ..., ff_coeffs[filter_dim]
+  coil::vstring torque_filter_params = coil::split(prop["torque_filter_params"], ","); // filter values
+  int filter_dim;
+  coil::stringTo(filter_dim, torque_filter_params[0].c_str());
+  std::cerr << "filter dim: " << filter_dim << std::endl;
+  // ex) 2dim butterworth filter sampling = 200[hz] cutoff = 5[hz]
+  // octave$ [a, b] = butter(2, 5/200)
+  // fb_coeffs[0] = 1.00000; <- b0
+  // fb_coeffs[1] = 1.88903; <- -b1
+  // fb_coeffs[2] = -0.89487; <- -b2
+  // ff_coeffs[0] = 0.0014603; <- a0
+  // ff_coeffs[1] = 0.0029206; <- a1
+  // ff_coeffs[2] = 0.0014603; <- a2
+  std::vector<double> fb_coeffs(filter_dim + 1);
+  std::vector<double> ff_coeffs(filter_dim + 1);
+  for(int i = 0; i < filter_dim + 1; i++){
+    coil::stringTo(fb_coeffs[i], torque_filter_params[i + 1].c_str());
+    coil::stringTo(ff_coeffs[i], torque_filter_params[i + (filter_dim + 2)].c_str());    
+    std::cerr << "fb[" << i << "]: " << fb_coeffs[i] << std::endl;
+    std::cerr << "ff[" << i << "]: " << ff_coeffs[i] << std::endl;
+  }
+  for(int i = 0; i < m_robot->numJoints(); i++){
+    m_filters.push_back(IIRFilter(filter_dim, fb_coeffs, ff_coeffs));
+  }
+  
   return RTC::RTC_OK;
 }
 
@@ -208,12 +248,12 @@ RTC::ReturnCode_t VirtualForceSensor::onExecute(RTC::UniqueId ec_id)
   if (m_qCurrentIn.isNew()) {
     m_qCurrentIn.read();
   }
-  if (m_tauIn.isNew()) {
-    m_tauIn.read();
+  if (m_tauInIn.isNew()) {
+    m_tauInIn.read();
   }
   if ( m_qRef.data.length() ==  m_robot->numJoints() &&
        m_qCurrent.data.length() ==  m_robot->numJoints() &&
-       m_tau.data.length() ==  m_robot->numJoints() ) {
+       m_tauIn.data.length() ==  m_robot->numJoints() ) {
     // reference model
     for ( int i = 0; i < m_robot->numJoints(); i++ ){
       m_robot->joint(i)->q = m_qCurrent.data[i];
@@ -232,6 +272,10 @@ RTC::ReturnCode_t VirtualForceSensor::onExecute(RTC::UniqueId ec_id)
       hrp::dmatrix Jtinv(6, n);
       path->calcJacobian(J);
       hrp::calcPseudoInverse(J.transpose(), Jtinv);
+      // use sr inverse of J.transpose()
+      // hrp::dmatrix Jt = J.transpose();
+      // double manipulability = sqrt((Jt*J).determinant());
+      // hrp::calcPseudoInverse((Jt * J + 0.1 * hrp::dmatrix::Identity(n,n)), Jtinv);
       hrp::dvector torque(n);
       hrp::dvector force(6);
 
@@ -245,7 +289,7 @@ RTC::ReturnCode_t VirtualForceSensor::onExecute(RTC::UniqueId ec_id)
         std::cerr << "  sensor name  : " << it->first << std::endl;
         std::cerr << "sensor torque  : ";
         for (int j = 0; j < n; j++) {
-          std::cerr << " " << m_tau.data[path->joint(j)->jointId] ;
+          std::cerr << " " << m_tauIn.data[path->joint(j)->jointId] ;
         }
         std::cerr << std::endl;
         std::cerr << "  calc torque  : ";
@@ -253,21 +297,29 @@ RTC::ReturnCode_t VirtualForceSensor::onExecute(RTC::UniqueId ec_id)
           std::cerr << " " << joint_torque(j);
         }
         std::cerr << std::endl;
-      }
-      
+      }     
+
       for (int j = 0; j < n; j++) {
         // torque calculation from electric current
-        //torque[j] = m_tau.data[path->joint(j)->jointId] -
-        
+        // torque[j] = m_tauIn.data[path->joint(j)->jointId] - joint_torque(j);
+        // torque[j] = m_filters[path->joint(j)->jointId].executeFilter(m_tauIn.data[path->joint(j)->jointId]) - joint_torque(j); // use filtered tau
+        torque[j] = -m_filters[path->joint(j)->jointId].executeFilter(m_tauIn.data[path->joint(j)->jointId]) + m_torque_offset[path->joint(j)->jointId];
+
         // torque calclation from error of joint angle
-        if ( m_error_to_torque_gain[path->joint(j)->jointId] == 0.0
-             || fabs(joint_error(j)) < m_error_dead_zone[path->joint(j)->jointId] ) {
-          torque[j] = 0;
-        } else {
-          torque[j] = error2torque(j, fabs(m_error_dead_zone[path->joint(j)->jointId])) - joint_torque(j);
-        }
-        
+        // if ( m_error_to_torque_gain[path->joint(j)->jointId] == 0.0
+        //      || fabs(joint_error(j)) < m_error_dead_zone[path->joint(j)->jointId] ) {
+        //   torque[j] = 0;
+        // } else {
+        //   torque[j] = error2torque(j, fabs(m_error_dead_zone[path->joint(j)->jointId]));
+        // }
+
+        // output without gravity compensation
+        m_tauOut.data[path->joint(j)->jointId] = torque[j];
+
+        // gravity compensation
+        torque[j] -= joint_torque(j);
       }
+      
       if ( DEBUGP ) {
         std::cerr << "row gear torque: ";
         for (int j = 0; j < n; j++) {
@@ -320,6 +372,7 @@ RTC::ReturnCode_t VirtualForceSensor::onExecute(RTC::UniqueId ec_id)
 
       m_force[i].tm = tm; // put timestamp
       m_forceOut[i]->write();
+      m_tauOutOut.write();
 
       it++; i++;
     }
