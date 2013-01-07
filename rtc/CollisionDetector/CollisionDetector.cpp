a8cc74ce54d5abf185438401a9a86f392063c65a
Kei OKADA <kei.okada@gmail.com>
2013-01-07 11:53:38 +0000

    fix warning message, collision detector through the motion command even if collision_pair is not defined
    git-svn-id: https://hrpsys-base.googlecode.com/svn/tags/3.1.3@612 a991ac11-fb38-5095-8c12-a1ddb0715245

--- rtc/CollisionDetector/CollisionDetector.cpp
+++ rtc/CollisionDetector/CollisionDetector.cpp
@@ -313,7 +313,7 @@ RTC::ReturnCode_t CollisionDetector::onExecute(RTC::UniqueId ec_id)
                     << " " << m_q.data[2] << " " << m_q.data[3] << " " << m_q.data[4] << " " << m_q.data[5] << " " << m_q.data[6] << std::endl;
         }
         if ( m_pair.size() == 0 && ( DEBUGP || loop % 100 == 1) ) {
-            std::cerr << "CollisionDetector is not working... not collision pair is defined, define collision_pair in configuration file" << std::endl;
+            std::cerr << "CAUTION!! The robot is moving without checking self collision detection!!! please define collision_pair in configuration file" << std::endl;
         }
         //
         m_qOut.write();
