bff2e86e45358d17fdfd25d3ed85f58c2d4dcb15
Kei OKADA <kei.okada@gmail.com>
2013-08-08 06:10:17 +0000

    remove wrong commit 813-814
    git-svn-id: https://hrpsys-base.googlecode.com/svn/trunk@816 a991ac11-fb38-5095-8c12-a1ddb0715245

--- rtc/Stabilizer/TwoDofController.h
+++ rtc/Stabilizer/TwoDofController.h
@@ -14,14 +14,14 @@
 
 class TwoDofController {
 public:
-  TwoDofController(double _ke = 0, double _tc = 0, double _dt = 0);
+  TwoDofController(double _ke =0, double _tc=0, double _dt=0);
   ~TwoDofController();
   void reset();
   void setup(double _ke, double _tc, double _dt);
   double update(double _x, double _xd);
 private:
   double ke, tc, dt; // ke: gain, tc: time constant, dt: control cycle
-  double integrated_diff; // integrated (xd - x)
+  double pdq;
 };
 
 #endif // TWO_DOF_CONTROLLER_H
