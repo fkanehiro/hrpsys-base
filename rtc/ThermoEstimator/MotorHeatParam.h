274994b7d0d6c6f2bab01f9464d3ad8a8d511dc3
Kei OKADA <kei.okada@gmail.com>
2013-08-08 05:27:28 +0000

    forge to add MotorHeatParam.h add ThermoEstimator/ThermoLimiter Issue 120
    git-svn-id: https://hrpsys-base.googlecode.com/svn/trunk@815 a991ac11-fb38-5095-8c12-a1ddb0715245

---
+++ rtc/ThermoEstimator/MotorHeatParam.h
@@ -0,0 +1,78 @@
+// -*- C++ -*-
+/*!
+ * @file  MotorHeatParam.h
+ * @brief motor heat parmaeter structure
+ * @date  $Date$
+ *
+ * $Id$
+ */
+
+#ifndef MOTOR_HEAT_PARAM_H
+#define MOTOR_HEAT_PARAM_H
+
+
+// </rtc-template>
+class MotorHeatParam
+{
+ public:
+  // Tnew = T + (P - ((T - Ta) / R) * dt) / C
+  //      = T + ((Re*K^2/C) * tau^2) + ((1/RC) * (T - Ta) * dt)
+  // * P = Re * I^2 = Re * (K * tau)^2
+  
+  double tempreture; // current tempreture
+  double currentCoeffs; // Re*K^2/C
+  double thermoCoeffs; // 1/RC
+
+  MotorHeatParam(){
+    defaultParams();
+  }
+  
+  // default params for motor heat param
+  void defaultParams(){
+    tempreture = 30.0;
+    currentCoeffs = 0.00003;
+    thermoCoeffs = 0.001;
+  } 
+
+};
+
+#endif // MOTOR_HEAT_PARAM_H
+// -*- C++ -*-
+/*!
+ * @file  MotorHeatParam.h
+ * @brief motor heat parmaeter structure
+ * @date  $Date$
+ *
+ * $Id$
+ */
+
+#ifndef MOTOR_HEAT_PARAM_H
+#define MOTOR_HEAT_PARAM_H
+
+
+// </rtc-template>
+class MotorHeatParam
+{
+ public:
+  // Tnew = T + (P - ((T - Ta) / R) * dt) / C
+  //      = T + ((Re*K^2/C) * tau^2) + ((1/RC) * (T - Ta) * dt)
+  // * P = Re * I^2 = Re * (K * tau)^2
+  
+  double tempreture; // current tempreture
+  double currentCoeffs; // Re*K^2/C
+  double thermoCoeffs; // 1/RC
+
+  MotorHeatParam(){
+    defaultParams();
+  }
+  
+  // default params for motor heat param
+  void defaultParams(){
+    tempreture = 30.0;
+    currentCoeffs = 0.00003;
+    thermoCoeffs = 0.001;
+  } 
+
+};
+
+#endif // MOTOR_HEAT_PARAM_H
