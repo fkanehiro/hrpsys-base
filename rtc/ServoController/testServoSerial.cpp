b4c8c5ac8c5ca88577d60ff4b671c7e1fd2695c6
Kei OKADA <kei.okada@gmail.com>
2013-08-29 03:15:55 +0000

    clear packet when id has changed
    git-svn-id: https://hrpsys-base.googlecode.com/svn/trunk@830 a991ac11-fb38-5095-8c12-a1ddb0715245

--- rtc/ServoController/testServoSerial.cpp
+++ rtc/ServoController/testServoSerial.cpp
@@ -69,6 +69,7 @@ int main() {
             case '8':
             case '9':
                 id = ch-'0';
+                serial->clear_packet();
                 break;
             case 'r':
                 serial->setReset(id);
