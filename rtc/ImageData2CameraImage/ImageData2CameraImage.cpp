26dc4e499f7dd46cffa816532389d54822c1d134
Fumio KANEHIRO <fkanehiro@gmail.com>
2014-02-07 11:55:22 +0000

    add a component ImageData2TimedcameraImage
    git-svn-id: https://hrpsys-base.googlecode.com/svn/trunk@979 a991ac11-fb38-5095-8c12-a1ddb0715245

---
+++ rtc/ImageData2CameraImage/ImageData2CameraImage.cpp
@@ -0,0 +1,169 @@
+// -*- C++ -*-
+/*!
+ * @file  ImageData2CameraImage.cpp
+ * @brief null component
+ * $Date$
+ *
+ * $Id$
+ */
+
+#include "ImageData2CameraImage.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* imagedata2cameraimage_spec[] =
+  {
+    "implementation_id", "ImageData2CameraImage",
+    "type_name",         "ImageData2CameraImage",
+    "description",       "imagedata2cameraimage component",
+    "version",           "1.0",
+    "vendor",            "AIST",
+    "category",          "example",
+    "activity_type",     "DataFlowComponent",
+    "max_instance",      "10",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+
+    ""
+  };
+// </rtc-template>
+
+ImageData2CameraImage::ImageData2CameraImage(RTC::Manager* manager)
+  : RTC::DataFlowComponentBase(manager),
+    // <rtc-template block="initializer">
+    m_dataIn("imageIn", m_data.data.image),
+    m_dataOut("imageOut", m_data),
+    // </rtc-template>
+    dummy(0)
+{
+}
+
+ImageData2CameraImage::~ImageData2CameraImage()
+{
+}
+
+
+
+RTC::ReturnCode_t ImageData2CameraImage::onInitialize()
+{
+  std::cout << m_profile.instance_name << ": onInitialize()" << std::endl;
+  // <rtc-template block="bind_config">
+  // Bind variables and configuration variable
+  
+  // </rtc-template>
+
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("imageIn", m_dataIn);
+
+  // Set OutPort buffer
+  addOutPort("imageOut", m_dataOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  return RTC::RTC_OK;
+}
+
+
+
+/*
+RTC::ReturnCode_t ImageData2CameraImage::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ImageData2CameraImage::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ImageData2CameraImage::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+RTC::ReturnCode_t ImageData2CameraImage::onActivated(RTC::UniqueId ec_id)
+{
+  std::cout << m_profile.instance_name<< ": onActivated(" << ec_id << ")" << std::endl;
+  return RTC::RTC_OK;
+}
+
+RTC::ReturnCode_t ImageData2CameraImage::onDeactivated(RTC::UniqueId ec_id)
+{
+  std::cout << m_profile.instance_name<< ": onDeactivated(" << ec_id << ")" << std::endl;
+  return RTC::RTC_OK;
+}
+
+RTC::ReturnCode_t ImageData2CameraImage::onExecute(RTC::UniqueId ec_id)
+{
+  if (m_dataIn.isNew()){
+    m_dataIn.read();
+    m_dataOut.write();
+  }
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t ImageData2CameraImage::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ImageData2CameraImage::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ImageData2CameraImage::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ImageData2CameraImage::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ImageData2CameraImage::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+
+  void ImageData2CameraImageInit(RTC::Manager* manager)
+  {
+    RTC::Properties profile(imagedata2cameraimage_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<ImageData2CameraImage>,
+                             RTC::Delete<ImageData2CameraImage>);
+  }
+
+};
+
+
