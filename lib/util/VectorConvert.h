03ec80d8a272a0f3388dc2643da205eb26f46c15
Fumio KANEHIRO <fkanehiro@gmail.com>
2014-03-16 13:35:06 +0000

    adds operator>> for hrp::dvector and hrp::Vector3
    git-svn-id: https://hrpsys-base.googlecode.com/svn/trunk@1021 a991ac11-fb38-5095-8c12-a1ddb0715245

--- lib/util/VectorConvert.h
+++ lib/util/VectorConvert.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <coil/stringutil.h>
+#include <hrpUtil/EigenTypes.h>
 
 template<typename T>
 std::istream& operator>>(std::istream& is, std::vector<T>& v)
@@ -17,6 +18,41 @@ std::istream& operator>>(std::istream& is, std::vector<T>& v)
   for (int i(0), len(sv.size()); i < len; ++i)
     {
       T tv;
+      if (coil::stringTo(tv, sv[i].c_str()))
+	{
+	  v[i] = tv;
+	}
+    }
+  return is;
+}
+
+std::istream& operator>>(std::istream& is, hrp::dvector& v)
+{
+  std::string s;
+  std::vector<std::string> sv;
+  is >> s;
+  sv = coil::split(s ,",");
+  v.resize(sv.size());
+  for (int i(0), len(sv.size()); i < len; ++i)
+    {
+      double tv;
+      if (coil::stringTo(tv, sv[i].c_str()))
+	{
+	  v[i] = tv;
+	}
+    }
+  return is;
+}
+
+std::istream& operator>>(std::istream& is, hrp::Vector3& v)
+{
+  std::string s;
+  std::vector<std::string> sv;
+  is >> s;
+  sv = coil::split(s ,",");
+  for (int i(0); i < 3; ++i)
+    {
+      double tv;
       if (coil::stringTo(tv, sv[i].c_str()))
 	{
 	  v[i] = tv;
