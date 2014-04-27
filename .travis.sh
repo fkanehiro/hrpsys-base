0de7018d4a0c313c4b281804ca6c7328a78f9b1d
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-04-28 00:46:51 +0900

    fix deb package for 315.1.x compatibility

--- .travis.sh
+++ .travis.sh
@@ -42,6 +42,42 @@ case $TEST_PACKAGE in
 
         # this is hotfix
         sudo wget https://raw.githubusercontent.com/start-jsk/rtmros_common/master/hrpsys_tools/test/test-pa10.test -O /opt/ros/hydro/share/hrpsys_tools/test/test-pa10.test
+
+        #
+        sed -i s@imu_floor@odom@g /opt/ros/hydro/share/hrpsys_ros_bridge/test/test-samplerobot.py
+
+        #
+        patch -p0 /opt/ros/hydro/share/hrpsys_ros_bridge/scripts/sensor_ros_bridge_connect.py <<EOF
+--- /opt/ros/hydro/share/hrpsys_ros_bridge/scripts/sensor_ros_bridge_connect.py 2014-04-17 17:28:42.000000000 +0900
++++ /opt/ros/hydro/share/hrpsys_ros_bridge/scripts/sensor_ros_bridge_connect.py 2014-04-28 00:30:27.250839313 +0900
+@@ -21,7 +21,7 @@
+             if rh.port(sen.name) != None: # check existence of sensor ;; currently original HRP4C.xml has different naming rule of gsensor and gyrometer
+                 print program_name, "connect ", sen.name, rh.port(sen.name).get_port_profile().name, bridge.port(sen.name).get_port_profile().name
+                 connectPorts(rh.port(sen.name), bridge.port(sen.name), "new")
+-                if sen.type == 'Force':
++                if sen.type == 'Force' and afs != None:
+                     print program_name, "connect ", sen.name, afs.port("off_" + sen.name).get_port_profile().name, bridge.port("off_" + sen.name).get_port_profile().name
+                     connectPorts(afs.port("off_" + sen.name), bridge.port("off_" + sen.name), "new") # for abs forces
+         else:
+
+EOF
+
+        patch -p0 /opt/ros/hydro/share/hrpsys_ros_bridge/launch/hrpsys_ros_bridge.launch <<EOF
+--- /opt/ros/hydro/share/hrpsys_ros_bridge/launch/hrpsys_ros_bridge.launch 2014-04-17 17:28:42.000000000 +0900
++++ /opt/ros/hydro/share/hrpsys_ros_bridge/launch/hrpsys_ros_bridge.launch 2014-04-28 00:30:27.250839313 +0900
+@@ -10,9 +10,9 @@
+   <!-- Set these values false when using HIRO -->
+   <arg name="USE_COMMON" default="true" />
+   <arg name="USE_ROBOTHARDWARE" default="false" />
+-  <arg name="USE_WALKING" default="true" />
++  <arg name="USE_WALKING" default="false" />
+   <arg name="USE_COLLISIONCHECK" default="true" />
+-  <arg name="USE_IMPEDANCECONTROLLER" default="true" />
++  <arg name="USE_IMPEDANCECONTROLLER" default="false" />
+   <arg name="USE_GRASPCONTROLLER" default="false" />
+   <arg name="USE_TORQUECONTROLLER" default="false" />
+   <arg name="USE_SOFTERRORLIMIT" default="true" />
+EOF
         #
         source /opt/ros/hydro/setup.bash
 
