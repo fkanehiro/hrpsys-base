f475c960ee09f930d748a5dff5468ed4f57f07a4
iori <iori@jsk.t.u-tokyo.ac.jp>
2014-05-28 16:08:36 +0900

    Modified testMotorTorqueController for TwoDofControllerWithDamper and change joint angle control model along with control model

--- rtc/TorqueController/testMotorTorqueController.cpp
+++ rtc/TorqueController/testMotorTorqueController.cpp
@@ -6,59 +6,57 @@
 #define ABS(x) (((x) < 0) ? (-(x)) : (x))
 
 int main (int argc, char* argv[]) {
-  double ke = 100, tc = 1.0, dt = 0.005;
-  MotorTorqueController controller("hoge", ke, tc, dt);
-  double q = 0, dq = 0, q_ref = 0, tau = 0, tau_d = 10.0, limit = 90.0;
-  double qgain = 0.000001;
+  double ke = 400.0, kd = -4.0, tc = 0.01, dt = 0.001;
+  const int test_num = 2;
+  MotorTorqueController *controller[test_num];
+  controller[0] = new MotorTorqueController("hoge", ke, tc, dt);
+  controller[1] = new MotorTorqueController("hoge", ke, kd, tc, dt);
+  double q[test_num], dq[test_num], q_ref[test_num], tau[test_num], dqref[test_num], ddqref[test_num];
+  double tau_d = 10.0, limit = 90.0, pgain = 1.0, dgain = 0.01;
+  bool activate_flag[test_num], stop_flag[test_num];
 
-  if (argc > 1) {
-    qgain = atof(argv[1]);
-  }
-  if (argc > 2) {
-    tau_d = atof(argv[2]);
-  }
-  if (argc > 3) {
-    limit = atof(argv[3]);
+  // initialize
+  for (int i = 0; i < test_num; i++){
+    q[i] = dq[i] = q_ref[i] = tau[i] = dqref[i] = ddqref[i] = 0;
+    activate_flag[i] = stop_flag[i] = false;
   }
 
   double time = 0;
-  bool activate_flag = false; 
-  bool stop_flag = false;
-  bool state1_flag = false;
   while ( true ) {
-    double tmp_q_ref = q_ref;
-    controller.setReferenceTorque(tau_d);
+    for (int i = 0; i < test_num; i++) {
+      controller[i]->setReferenceTorque(tau_d);
 
-    if (time > 5.0 && activate_flag == false) {
-      controller.activate();
-      std::cout << "#activated" << std::endl;
-      activate_flag = true;
+      if (time > 5.0 && activate_flag[i] == false) {
+        controller[i]->activate();
+        std::cerr << "#activated[" << i << "]" << std::endl;
+        activate_flag[i] = true;
+      }
+
+      // execute controller
+      tau[i] = -ke * q[i] + kd * (ddqref[i] / dt);
+      dqref[i] = controller[i]->execute(tau[i], limit); // execute returns dqRef = sum(dq)
+      ddqref[i] = dqref[i] - q[i];
+
+      // joint simulation
+      q[i] = dqref[i];
+
+      if (activate_flag[i] == true) {
+        if (time > 20 && stop_flag[i] != true) {
+          controller[i]->deactivate();
+          stop_flag[i] = true;
+        } else if (time > 10) {
+          tau_d = 5.0;
+        }
+      }
+      if (time > 30) {
+        for (int i = 0; i < test_num; i++) {
+          delete controller[i];
+        }
+        return 0;
+      }
     }
-
-    // execute controller
-    dq = controller.execute(tau, limit);
-    tmp_q_ref -= dq;
-
-    // joint simulation
-    tau = -ke * (q - tmp_q_ref);
-    q += qgain * (tmp_q_ref - q);
-
-    if (ABS(tau - tau_d) < 0.1 && state1_flag == false && time > 100) {
-      tau_d = 5.0;
-      state1_flag = true;
-      std::cout << "#taud changed" << std::endl;
-    } else if (ABS(tau - tau_d) < 0.1 && stop_flag == false && time > 200) {
-      controller.deactivate();
-      stop_flag = true;
-      std::cout << "#stopped" << std::endl;
-    }
-    std::cout << time << " " << q << " " << dq << " " << tau << " " << tau_d << std::endl;
+    std::cout << time << " " << q[0] << " " << dqref[0] << " " << tau[0] << " " << q[1] << " " << dqref[1] << " " << tau[1] << " " << tau_d << std::endl;
     time += dt;
-
-    if((stop_flag == true && ABS(q) < 0.0001) || time > 500){
-      return 0;
-    }
-    
   }
   return 0;
 }
