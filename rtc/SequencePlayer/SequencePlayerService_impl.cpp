4aade831d1c8970d363c2ded0cb035cb8187671c
emijah.s@gmail.com <emijah.s@gmail.com>
2014-06-27 19:35:45 +0900

    added interface for setTargetPoseMatrix

--- rtc/SequencePlayer/SequencePlayerService_impl.cpp
+++ rtc/SequencePlayer/SequencePlayerService_impl.cpp
@@ -13,7 +13,7 @@ SequencePlayerService_impl::~SequencePlayerService_impl()
 
 void SequencePlayerService_impl::waitInterpolation()
 {
-  m_player->waitInterpolation();
+    m_player->waitInterpolation();
 }
 
 CORBA::Boolean SequencePlayerService_impl::waitInterpolationOfGroup(const char *gname)
@@ -23,17 +23,17 @@ CORBA::Boolean SequencePlayerService_impl::waitInterpolationOfGroup(const char *
 
 CORBA::Boolean SequencePlayerService_impl::setJointAngles(const dSequence& jvs, CORBA::Double tm)
 {
-  if (jvs.length() != (unsigned int)(m_player->robot()->numJoints())) {
-      std::cerr << __PRETTY_FUNCTION__ << " num of joint is differ, input:" << jvs.length() << ", robot:" << (unsigned int)(m_player->robot()->numJoints()) << std::endl;
-      return false;
-  }
-  return m_player->setJointAngles(jvs.get_buffer(), tm);
+    if(jvs.length() != (unsigned int)(m_player->robot()->numJoints())) {
+        std::cerr << __PRETTY_FUNCTION__ << " num of joint is differ, input:" << jvs.length() << ", robot:" << (unsigned int)(m_player->robot()->numJoints()) << std::endl;
+        return false;
+    }
+    return m_player->setJointAngles(jvs.get_buffer(), tm);
 }
 
 CORBA::Boolean SequencePlayerService_impl::setJointAnglesWithMask(const dSequence& jvs, const bSequence& mask, CORBA::Double tm)
 {
-    if (jvs.length() != (unsigned int)(m_player->robot()->numJoints())
-        || mask.length() != (unsigned int)(m_player->robot()->numJoints())) {
+    if(jvs.length() != (unsigned int)(m_player->robot()->numJoints())
+       || mask.length() != (unsigned int)(m_player->robot()->numJoints())) {
         std::cerr << __PRETTY_FUNCTION__ << " num of joint is differ, input:" << jvs.length() << ", mask:" << mask.length() << ", robot" << (unsigned int)(m_player->robot()->numJoints()) << std::endl;
         return false;
     }
@@ -45,7 +45,7 @@ CORBA::Boolean SequencePlayerService_impl::setJointAngle(const char *jname, CORB
 {
     BodyPtr r = m_player->robot();
     Link *l = r->link(jname);
-    if (!l){
+    if(!l) {
         std::cerr << "can't find(" << jname << ")" << std::endl;
         return false;
     }
@@ -55,85 +55,94 @@ CORBA::Boolean SequencePlayerService_impl::setJointAngle(const char *jname, CORB
 
 CORBA::Boolean SequencePlayerService_impl::setBasePos(const dSequence& pos, CORBA::Double tm)
 {
-    if (pos.length() != 3) return false;
+    if(pos.length() != 3) return false;
 
     return m_player->setBasePos(pos.get_buffer(), tm);
 }
 
 CORBA::Boolean SequencePlayerService_impl::setBaseRpy(const dSequence& rpy, CORBA::Double tm)
 {
-    if (rpy.length() != 3) return false;
+    if(rpy.length() != 3) return false;
 
     return m_player->setBaseRpy(rpy.get_buffer(), tm);
 }
 
 CORBA::Boolean SequencePlayerService_impl::setZmp(const dSequence& zmp, CORBA::Double tm)
 {
-    if (zmp.length() != 3) return false;
+    if(zmp.length() != 3) return false;
 
     return m_player->setZmp(zmp.get_buffer(), tm);
 }
 
 CORBA::Boolean SequencePlayerService_impl::setWrenches(const dSequence& wrenches, CORBA::Double tm)
 {
-  //if (wrenches.length() != ) return false;
+    //if (wrenches.length() != ) return false;
 
     return m_player->setWrenches(wrenches.get_buffer(), tm);
 }
 
-CORBA::Boolean SequencePlayerService_impl::setTargetPose(const char* gname, const dSequence& xyz, const dSequence& rpy, CORBA::Double tm){
+CORBA::Boolean SequencePlayerService_impl::setTargetPose(const char* gname, const dSequence& xyz, const dSequence& rpy, CORBA::Double tm) {
     char* frame_name = (char *)strrchr(gname, ':');
-    if ( frame_name ) {
+    if(frame_name) {
         ((char *)gname)[frame_name - gname] = '\0'; // cut frame_name, gname[strpos(':')] = 0x00
         frame_name++; // skip ":"
     }
     return m_player->setTargetPose(gname, xyz.get_buffer(), rpy.get_buffer(), tm, frame_name);
 }
 
+CORBA::Boolean SequencePlayerService_impl::setTargetPoseMatrix(const char* gname, const dSequence& xyz, const dSequence& rot, CORBA::Double tm) {
+    char* frame_name = (char *)strrchr(gname, ':');
+    if(frame_name) {
+        ((char *)gname)[frame_name - gname] = '\0'; // cut frame_name, gname[strpos(':')] = 0x00
+        frame_name++; // skip ":"
+    }
+    return m_player->setTargetPose(gname, xyz.get_buffer(), rot.get_buffer(), tm, frame_name);
+}
+
 CORBA::Boolean SequencePlayerService_impl::isEmpty()
 {
-  return m_player->player()->isEmpty();
+    return m_player->player()->isEmpty();
 }
 
 void SequencePlayerService_impl::loadPattern(const char* basename, CORBA::Double tm)
 {
-  if (!m_player->player()){
-    std::cerr << "player is not set"<< std::endl;
-    return;
-  }
-  m_player->loadPattern(basename, tm);
+    if(!m_player->player()) {
+        std::cerr << "player is not set" << std::endl;
+        return;
+    }
+    m_player->loadPattern(basename, tm);
 }
 
 void SequencePlayerService_impl::clear()
 {
-  m_player->player()->clear();
+    m_player->player()->clear();
 }
 
-CORBA::Boolean  SequencePlayerService_impl::clearOfGroup(const char *gname, CORBA::Double i_limitation)
+CORBA::Boolean SequencePlayerService_impl::clearOfGroup(const char *gname, CORBA::Double i_limitation)
 {
     m_player->player()->clearOfGroup(gname, i_limitation);
 }
 
 void SequencePlayerService_impl::clearNoWait()
 {
-  m_player->setClearFlag();
+    m_player->setClearFlag();
 }
 
 CORBA::Boolean SequencePlayerService_impl::setInterpolationMode(OpenHRP::SequencePlayerService::interpolationMode i_mode_)
 {
-  return m_player->setInterpolationMode(i_mode_);
+    return m_player->setInterpolationMode(i_mode_);
 }
 
 CORBA::Boolean SequencePlayerService_impl::setInitialState()
 {
-  m_player->setInitialState();
-  return m_player->setInitialState(m_player->dt);
+    m_player->setInitialState();
+    return m_player->setInitialState(m_player->dt);
 }
 
 void SequencePlayerService_impl::player(SequencePlayer *i_player)
 {
-  m_player = i_player;
-} 
+    m_player = i_player;
+}
 
 void SequencePlayerService_impl::playPattern(const dSequenceSequence& pos, const dSequenceSequence& rpy, const dSequenceSequence& zmp, const dSequence& tm)
 {
--- rtc/SequencePlayer/SequencePlayerService_impl.h
+++ rtc/SequencePlayer/SequencePlayerService_impl.h
@@ -8,41 +8,42 @@ using namespace OpenHRP;
 
 class SequencePlayer;
 
-class SequencePlayerService_impl 
-  : public virtual POA_OpenHRP::SequencePlayerService,
+class SequencePlayerService_impl
+    : public virtual POA_OpenHRP::SequencePlayerService,
     public virtual PortableServer::RefCountServantBase
 {
 public:
-  SequencePlayerService_impl();
-  virtual ~SequencePlayerService_impl();
-  //
-  void waitInterpolation();
-  CORBA::Boolean waitInterpolationOfGroup(const char *gname);
-  CORBA::Boolean setJointAngles(const dSequence& jvs, CORBA::Double tm);
-  CORBA::Boolean setJointAnglesWithMask(const dSequence& jvs, const bSequence& mask, CORBA::Double tm);
-  CORBA::Boolean setJointAngle(const char *jname, CORBA::Double jv, CORBA::Double tm);
-  CORBA::Boolean setBasePos(const dSequence& pos, CORBA::Double tm);
-  CORBA::Boolean setBaseRpy(const dSequence& rpy, CORBA::Double tm);
-  CORBA::Boolean setZmp(const dSequence& zmp, CORBA::Double tm);
-  CORBA::Boolean setWrenches(const dSequence& wrenches, CORBA::Double tm);
-  CORBA::Boolean setTargetPose(const char* gname, const dSequence& xyz, const dSequence& rpy, CORBA::Double tm);
-  CORBA::Boolean isEmpty();
-  void loadPattern(const char* basename, CORBA::Double tm);
-  void playPattern(const dSequenceSequence& pos, const dSequenceSequence& rpy, const dSequenceSequence& zmp, const dSequence& tm);
-  void clear();
-  void clearNoWait();
-  CORBA::Boolean setInterpolationMode(OpenHRP::SequencePlayerService::interpolationMode i_mode_);
-  CORBA::Boolean setInitialState();
-  CORBA::Boolean addJointGroup(const char* gname, const OpenHRP::SequencePlayerService::StrSequence& jnames);
-  CORBA::Boolean removeJointGroup(const char* gname);
-  CORBA::Boolean setJointAnglesOfGroup(const char *gname, const dSequence& jvs, CORBA::Double tm);
-  CORBA::Boolean clearOfGroup(const char *gname, CORBA::Double  i_timelimit);
-  CORBA::Boolean playPatternOfGroup(const char *gname, const dSequenceSequence& pos, const dSequence& tm);
-  void setMaxIKError(CORBA::Double pos, CORBA::Double rot);
-  void setMaxIKIteration(CORBA::Short iter);
-  //
-  void player(SequencePlayer *i_player);
-  SequencePlayer *m_player;
-};				 
+    SequencePlayerService_impl();
+    virtual ~SequencePlayerService_impl();
+    //
+    void waitInterpolation();
+    CORBA::Boolean waitInterpolationOfGroup(const char *gname);
+    CORBA::Boolean setJointAngles(const dSequence& jvs, CORBA::Double tm);
+    CORBA::Boolean setJointAnglesWithMask(const dSequence& jvs, const bSequence& mask, CORBA::Double tm);
+    CORBA::Boolean setJointAngle(const char *jname, CORBA::Double jv, CORBA::Double tm);
+    CORBA::Boolean setBasePos(const dSequence& pos, CORBA::Double tm);
+    CORBA::Boolean setBaseRpy(const dSequence& rpy, CORBA::Double tm);
+    CORBA::Boolean setZmp(const dSequence& zmp, CORBA::Double tm);
+    CORBA::Boolean setWrenches(const dSequence& wrenches, CORBA::Double tm);
+    CORBA::Boolean setTargetPose(const char* gname, const dSequence& xyz, const dSequence& rpy, CORBA::Double tm);
+    CORBA::Boolean setTargetPoseMatrix(const char* gname, const dSequence& xyz, const dSequence& rot, CORBA::Double tm);
+    CORBA::Boolean isEmpty();
+    void loadPattern(const char* basename, CORBA::Double tm);
+    void playPattern(const dSequenceSequence& pos, const dSequenceSequence& rpy, const dSequenceSequence& zmp, const dSequence& tm);
+    void clear();
+    void clearNoWait();
+    CORBA::Boolean setInterpolationMode(OpenHRP::SequencePlayerService::interpolationMode i_mode_);
+    CORBA::Boolean setInitialState();
+    CORBA::Boolean addJointGroup(const char* gname, const OpenHRP::SequencePlayerService::StrSequence& jnames);
+    CORBA::Boolean removeJointGroup(const char* gname);
+    CORBA::Boolean setJointAnglesOfGroup(const char *gname, const dSequence& jvs, CORBA::Double tm);
+    CORBA::Boolean clearOfGroup(const char *gname, CORBA::Double i_timelimit);
+    CORBA::Boolean playPatternOfGroup(const char *gname, const dSequenceSequence& pos, const dSequence& tm);
+    void setMaxIKError(CORBA::Double pos, CORBA::Double rot);
+    void setMaxIKIteration(CORBA::Short iter);
+    //
+    void player(SequencePlayer *i_player);
+    SequencePlayer *m_player;
+};
 
 #endif
