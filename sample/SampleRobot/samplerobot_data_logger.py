898af9c331f2e06222b7f0c67135dd9f2e2fe306
Shunichi Nozawa <notheworld@gmail.com>
2014-07-11 10:27:00 +0900

    (SampleRobot) : Add examples for samplerobot by copying from start-jsk/hrpsys/samples discussed in https://github.com/fkanehiro/hrpsys-base/issues/240

---
+++ sample/SampleRobot/samplerobot_data_logger.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+try:
+    from hrpsys.hrpsys_config import *
+    import OpenHRP
+except:
+    print "import without hrpsys"
+    import rtm
+    from rtm import *
+    from OpenHRP import *
+    import waitInput
+    from waitInput import *
+    import socket
+    import time
+
+def getRTCList ():
+    return [
+            ['seq', "SequencePlayer"],
+            ['sh', "StateHolder"],
+            ['fk', "ForwardKinematics"],
+            ['log', "DataLogger"],
+            ]
+
+def init ():
+    global hcf
+    hcf = HrpsysConfigurator()
+    hcf.getRTCList = getRTCList
+    hcf.init ("SampleRobot(Robot)0")
+
+def demo ():
+    init()
+    # set initial pose from sample/controller/SampleController/etc/Sample.pos
+    initial_pose = [-7.779e-005,  -0.378613,  -0.000209793,  0.832038,  -0.452564,  0.000244781,  0.31129,  -0.159481,  -0.115399,  -0.636277,  0,  0,  0,  -7.77902e-005,  -0.378613,  -0.000209794,  0.832038,  -0.452564,  0.000244781,  0.31129,  0.159481,  0.115399,  -0.636277,  0,  0,  0,  0,  0,  0]
+
+    # Set max ring-buffer length : 200 [loop] * 0.002 [s] = 0.4 [s] data
+    hcf.log_svc.maxLength(200)
+    # Clear buffer
+    hcf.log_svc.clear()
+    hcf.seq_svc.setJointAngles(initial_pose, 2.0)
+    hcf.seq_svc.waitInterpolation()
+    # Save log files for each ports as /tmp/test-samplerobot-log.*
+    #   file names are /tmp/test-samplerobot-log.[RTCName]_[PortName], c.f.,  /tmp/test-samplerobot-log.sh_qOut ... etc
+    hcf.log_svc.save("/tmp/test-samplerobot-log")
---
+++ sample/SampleRobot/samplerobot_stabilizer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+try:
+    from hrpsys.hrpsys_config import *
+    import OpenHRP
+except:
+    print "import without hrpsys"
+    import rtm
+    from rtm import *
+    from OpenHRP import *
+    import waitInput
+    from waitInput import *
+    import socket
+    import time
+
+def getRTCList ():
+    return [
+            ['seq', "SequencePlayer"],
+            ['sh', "StateHolder"],
+            ['fk', "ForwardKinematics"],
+            ['kf', "KalmanFilter"],
+            ['rmfo', "RemoveForceSensorLinkOffset"],
+            ['abc', "AutoBalancer"],
+            ['st', "Stabilizer"],
+            ]
+
+def init ():
+    global hcf
+    hcf = HrpsysConfigurator()
+    hcf.getRTCList = getRTCList
+    hcf.init ("SampleRobot(Robot)0")
+
+def demo():
+    init()
+    # set initial pose from sample/controller/SampleController/etc/Sample.pos
+    initial_pose = [-7.779e-005,  -0.378613,  -0.000209793,  0.832038,  -0.452564,  0.000244781,  0.31129,  -0.159481,  -0.115399,  -0.636277,  0,  0,  0,  -7.77902e-005,  -0.378613,  -0.000209794,  0.832038,  -0.452564,  0.000244781,  0.31129,  0.159481,  0.115399,  -0.636277,  0,  0,  0,  0,  0,  0]
+    hcf.seq_svc.setJointAngles(initial_pose, 2.0)
+    hcf.seq_svc.waitInterpolation()
+
+    # s
+    hcf.st_svc.setParameter(OpenHRP.StabilizerService.stParam([0.5,0.5], [5.0, 5.0], [0.0,0.0], [0.1, 0.1], [0,0], [0,0], [0,0], [0,0], 0,0,0,0))
+    hcf.st_svc.startStabilizer ()
+    hcf.abc_svc.goPos(0.5, 0.1, 10)
+    hcf.abc_svc.waitFootSteps()
+    hcf.st_svc.stopStabilizer ()
