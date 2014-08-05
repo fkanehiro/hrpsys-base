91015c152d6ac0e23241af0007af2e6ab86a89ec
emijah <emijah.s@gmail.com>
2014-08-05 13:49:07 +0900

    fixed typo. fixed indents

--- rtc/SequencePlayer/SequencePlayer.h
+++ rtc/SequencePlayer/SequencePlayer.h
@@ -91,12 +91,8 @@ class SequencePlayer
   // virtual RTC::ReturnCode_t onRateChanged(RTC::UniqueId ec_id);
 
   double dt;
-  seqplay *player() {
-      return m_seq;
-  }
-  hrp::BodyPtr robot() {
-      return m_robot;
-  }
+  seqplay *player() { return m_seq; }
+  hrp::BodyPtr robot() { return m_robot; }
   void setClearFlag();
   void waitInterpolation();
   bool waitInterpolationOfGroup(const char *gname);
@@ -113,7 +109,7 @@ class SequencePlayer
   void loadPattern(const char *basename, double time);
   void playPattern(const OpenHRP::dSequenceSequence& pos, const OpenHRP::dSequenceSequence& rpy, const OpenHRP::dSequenceSequence& zmp, const OpenHRP::dSequence& tm);
   bool setInterpolationMode(OpenHRP::SequencePlayerService::interpolationMode i_mode_);
-  bool setInitialState(double tm = 0.0);
+  bool setInitialState(double tm=0.0);
   bool addJointGroup(const char *gname, const OpenHRP::SequencePlayerService::StrSequence& jnames);
   bool removeJointGroup(const char *gname);
   bool setJointAnglesOfGroup(const char *gname, const double *angles, double tm);
@@ -121,7 +117,7 @@ class SequencePlayer
 
   void setMaxIKError(double pos, double rot);
   void setMaxIKIteration(short iter);
-otected:
+ protected:
   // Configuration variable declaration
   // <rtc-template block="config_declare">
 
