ef571a8f00125f86fa48b3f76dc224af888246e6
Shunichi Nozawa <notheworld@gmail.com>
2014-11-05 07:18:38 +0900

    (SequencePlayer, samplerobot_impedance_controller.py.in) : Add setWrench for SequencePlayer and add new service function to the last of idl file.

--- rtc/SequencePlayer/SequencePlayerService_impl.cpp
+++ rtc/SequencePlayer/SequencePlayerService_impl.cpp
@@ -81,6 +81,13 @@ CORBA::Boolean SequencePlayerService_impl::setWrenches(const dSequence& wrenches
     return m_player->setWrenches(wrenches.get_buffer(), tm);
 }
 
+CORBA::Boolean SequencePlayerService_impl::setWrench(const char* name, const dSequence& wrench, CORBA::Double tm)
+{
+  //if (wrenches.length() != ) return false;
+
+    return m_player->setWrench(name, wrench.get_buffer(), tm);
+}
+
 CORBA::Boolean SequencePlayerService_impl::setTargetPose(const char* gname, const dSequence& xyz, const dSequence& rpy, CORBA::Double tm){
     char* frame_name = (char *)strrchr(gname, ':');
     if ( frame_name ) {
