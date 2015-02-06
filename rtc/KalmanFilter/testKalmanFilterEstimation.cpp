62aae85bb3c54d49561d02ccec9cd3e000d06b3b
Shunichi Nozawa <notheworld@gmail.com>
2015-02-06 17:40:17 +0900

    Add gnuplot and update comments

--- rtc/KalmanFilter/testKalmanFilterEstimation.cpp
+++ rtc/KalmanFilter/testKalmanFilterEstimation.cpp
@@ -13,7 +13,7 @@
 #include "EKFilter.h"
 
 // Usage
-// testKalmanFilterEstimation --acc-file /tmp/kftest/test.acc --rate-file /tmp/kftest/test.rate --pose-file /tmp/kftest/test.pose --Q-angle 1e-3 --Q-rate 1e-10 --dt 0.005 --R-angle 1
+// testKalmanFilterEstimation --acc-file /tmp/kftest/test.acc --rate-file /tmp/kftest/test.rate --pose-file /tmp/kftest/test.pose --Q-angle 1e-3 --Q-rate 1e-10 --dt 0.005 --R-angle 1 --use-gnuplot true
 
 int main(int argc, char *argv[])
 {
@@ -23,6 +23,7 @@ int main(int argc, char *argv[])
   double R_angle = 10;
   double dt = 0.002;
   std::string pose_file("test.pose"), acc_file("test.acc"), rate_file("test.rate");
+  bool use_gnuplot = false;
 
   // Parse argument
   for (int i = 0; i < argc; ++ i) {
@@ -41,16 +42,22 @@ int main(int argc, char *argv[])
           if (++i < argc) acc_file = argv[i];
       } else if ( arg == "--pose-file" ) { // File path for actual pose
           if (++i < argc) pose_file = argv[i];
+      } else if ( arg == "--use-gnuplot" ) { // Use gnuplot (true or false)
+          if (++i < argc) use_gnuplot = (std::string(argv[i])=="true"?true:false);
       }
   }
 
+  // Setup input and output files
   std::ifstream ratef(rate_file.c_str()), accf(acc_file.c_str()), posef(pose_file.c_str());
   std::cerr << "File : " << rate_file << " " << acc_file << " " << pose_file << std::endl;
   if (!ratef.is_open() || !accf.is_open() || !posef.is_open()) {
       std::cerr << "No such " << rate_file << " " << acc_file << " " << pose_file << std::endl;
       return -1;
   }
+  std::string ofname("/tmp/testKalmanFilterEstimation.dat");
+  std::ofstream ofs(ofname.c_str());
 
+  // Test kalman filter
   RPYKalmanFilter rpy_kf;
   rpy_kf.setParam(dt, Q_angle, Q_rate, R_angle);
   hrp::Vector3 rate, acc, rpy, rpyRaw, rpyAct;
@@ -60,7 +67,29 @@ int main(int argc, char *argv[])
     ratef >> time >> rate[0] >> rate[1] >> rate[2];
     accf >> time >> acc[0] >> acc[1] >> acc[2];
     rpy_kf.main_one(rpy, rpyRaw, acc, rate);
-    std::cout << rpy[0] << " " << rpy[1] << " " << rpy[2] << " " << rpyAct[0] << " " << rpyAct[1] << " " << rpyAct[2] << std::endl;
+    if (use_gnuplot) {
+        ofs << time << " " << rpy[0] << " " << rpy[1] << " " << rpy[2] << " " << rpyAct[0] << " " << rpyAct[1] << " " << rpyAct[2] << std::endl;
+    } else {
+        std::cout << rpy[0] << " " << rpy[1] << " " << rpy[2] << " " << rpyAct[0] << " " << rpyAct[1] << " " << rpyAct[2] << std::endl;
+    }
+  }
+  // gnuplot
+  if (use_gnuplot) {
+      FILE* gp[3];
+      std::string titles[3] = {"Roll", "Pitch", "Yaw"};
+      for (size_t ii = 0; ii < 3; ii++) {
+          gp[ii] = popen("gnuplot", "w");
+          fprintf(gp[ii], "set title \"%s\"\n", titles[ii].c_str());
+          fprintf(gp[ii], "set xlabel \"Time [s]\"\n");
+          fprintf(gp[ii], "set ylabel \"Attitude [rad]\"\n");
+          fprintf(gp[ii], "plot \"%s\" using 1:%d with lines title \"Estimated\"\n", ofname.c_str(), (2 + ii));
+          fprintf(gp[ii], "replot \"%s\" using 1:%d with lines title \"Actual\"\n", ofname.c_str(), (2 + ii + 3));
+          fflush(gp[ii]);
+      }
+      std::cout << "Type any keys + enter to exit." << std::endl;
+      double tmp;
+      std::cin >> tmp;
+      for (size_t j = 0; j < 3; j++) pclose(gp[j]);
   }
   return 0;
 }
