2955e38f1f546956014252b2f748db9471723d01
Shunichi Nozawa <notheworld@gmail.com>
2015-02-06 16:37:41 +0900

    Add comments to testKalmanFilterEstimation

--- rtc/KalmanFilter/testKalmanFilterEstimation.cpp
+++ rtc/KalmanFilter/testKalmanFilterEstimation.cpp
@@ -1,45 +1,62 @@
+// -*- C++ -*-
+/*!
+ * @file  testKalmanFilterEstimation.cpp
+ * @brief Test program for KalmanFilter.cpp requires input log data files
+ * @date  $Date$
+ *
+ * $Id$
+ */
+
+
 #include <fstream>
 #include "RPYKalmanFilter.h"
 #include "EKFilter.h"
 
+// Usage
+// testKalmanFilterEstimation --acc-file /tmp/kftest/test.acc --rate-file /tmp/kftest/test.rate --pose-file /tmp/kftest/test.pose --Q-angle 1e-3 --Q-rate 1e-10 --dt 0.005 --R-angle 1
+
 int main(int argc, char *argv[])
 {
+  // Default file names and params
   double Q_angle = 0.001;
   double Q_rate = 0.003;
   double R_angle = 10;
   double dt = 0.002;
   std::string pose_file("test.pose"), acc_file("test.acc"), rate_file("test.rate");
+
+  // Parse argument
   for (int i = 0; i < argc; ++ i) {
       std::string arg(argv[i]);
-      if ( arg == "--Q-angle" ) {
+      if ( arg == "--Q-angle" ) { // KF parameters
           if (++i < argc) Q_angle = atof(argv[i]);
       } else if ( arg == "--Q-rate" ) {
           if (++i < argc) Q_rate = atof(argv[i]);
       } else if ( arg == "--R-angle" ) {
           if (++i < argc) R_angle = atof(argv[i]);
-      } else if ( arg == "--dt" ) {
+      } else if ( arg == "--dt" ) { // sampling time[s]
           if (++i < argc) dt = atof(argv[i]);
-      } else if ( arg == "--rate-file" ) {
+      } else if ( arg == "--rate-file" ) { // File path for rate
           if (++i < argc) rate_file = argv[i];
-      } else if ( arg == "--acc-file" ) {
+      } else if ( arg == "--acc-file" ) { // File path for acc
           if (++i < argc) acc_file = argv[i];
-      } else if ( arg == "--pose-file" ) {
+      } else if ( arg == "--pose-file" ) { // File path for actual pose
           if (++i < argc) pose_file = argv[i];
       }
   }
+
   std::ifstream ratef(rate_file.c_str()), accf(acc_file.c_str()), posef(pose_file.c_str());
   std::cerr << "File : " << rate_file << " " << acc_file << " " << pose_file << std::endl;
   if (!ratef.is_open() || !accf.is_open() || !posef.is_open()) {
       std::cerr << "No such " << rate_file << " " << acc_file << " " << pose_file << std::endl;
       return -1;
   }
+
   RPYKalmanFilter rpy_kf;
   rpy_kf.setParam(dt, Q_angle, Q_rate, R_angle);
-
   hrp::Vector3 rate, acc, rpy, rpyRaw, rpyAct;
   double time;
   while(!ratef.eof()){
-    posef >> time >> time >> time >> time >> rpyAct[0] >> rpyAct[1] >> rpyAct[2];
+    posef >> time >> time >> time >> time >> rpyAct[0] >> rpyAct[1] >> rpyAct[2]; // Neglect translation in .pose file
     ratef >> time >> rate[0] >> rate[1] >> rate[2];
     accf >> time >> acc[0] >> acc[1] >> acc[2];
     rpy_kf.main_one(rpy, rpyRaw, acc, rate);
