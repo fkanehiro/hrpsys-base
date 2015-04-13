be71cab8556567d60e80eb7e4c354049dc787409
Kei Okada <kei.okada@gmail.com>
2015-04-13 18:49:50 +0900

    [launch/samplerobot-terrain-walk.{launch,py} add sample program for terrain walk

--- launch/samplerobot-terrain-walk.py
+++ launch/samplerobot-terrain-walk.py
@@ -4,30 +4,23 @@
  this is example file for SampleRobot robot terrain walking
 
  for SlopeUpDown
- $ roslaunch hrpsys samplerobot.launch CONTROLLER_PERIOD:=200 PROJECT_FILE:=`rospack find hrpsys`/share/hrpsys/samples/SampleRobot/SampleRobot.TerrainFloor.SlopeUpDown.xml
+ $ roslaunch hrpsys samplerobot-terrain-walk.launch MODEL:=SlopeUpDown
  $ rosrun hrpsys samplerobot-terrain-walk.py --SlopeUpDown
 
  for StairUp
- $ roslaunch hrpsys samplerobot.launch CONTROLLER_PERIOD:=200 PROJECT_FILE:=`rospack find hrpsys`/share/hrpsys/samples/SampleRobot/SampleRobot.TerrainFloor.StairUp.xml
+ $ roslaunch hrpsys samplerobot-terrain-walk.launch MODEL:=StairUp
  $ rosrun hrpsys samplerobot-terrain-walk.py --StairUp
 
  for StairDown
- $ roslaunch hrpsys samplerobot.launch CONTROLLER_PERIOD:=200 PROJECT_FILE:=`rospack find hrpsys`/share/hrpsys/samples/SampleRobot/SampleRobot.TerrainFloor.StairDown.xml
+ $ roslaunch hrpsys samplerobot-terrain-walk.launch MODEL:=StairDown
  $ rosrun hrpsys samplerobot-terrain-walk.py --StairDown
 
 """
 
 import imp, sys, os
 
-# set path to hrpsys to use HrpsysConfigurator
-try:
-    imp.find_module('hrpsys') # catkin installed
-    sys.path.append(imp.find_module('hrpsys')[1])
-except: # rosbuild installed
-    import roslib
-    roslib.load_manifest('hrpsys')
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/../share/hrpsys/samples/SampleRobot/') # set path to SampleRobot
+from subprocess import check_output
+sys.path.append(os.path.join(check_output(['pkg-config', 'hrpsys-base', '--variable=prefix']).rstrip(),'share/hrpsys/samples/SampleRobot/')) # set path to SampleRobot
 
 import samplerobot_terrain_walk
 
