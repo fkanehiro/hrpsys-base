c83ea87042f2044147b897f3e64672e598c0acb0
Shunichi Nozawa <notheworld@gmail.com>
2015-04-25 20:05:38 +0900

    Rename WC_RLEG => RLEG, WC_LLEG => LLEG

--- rtc/AutoBalancer/GaitGenerator.cpp
+++ rtc/AutoBalancer/GaitGenerator.cpp
@@ -16,9 +16,9 @@ namespace rats
   {
     hrp::Vector3 rzmp;
     hrp::Vector3 dz0, dz1, ret_zmp;
-    leg_type spl = (fnl[fs_index].l_r == WC_RLEG) ? WC_LLEG :WC_RLEG;
-    dz0 = _support_leg_coords.rot * ((spl == WC_RLEG)? default_zmp_offsets[0] : default_zmp_offsets[1]);
-    dz1 = _swing_leg_coords.rot * ((spl == WC_RLEG)? default_zmp_offsets[1] : default_zmp_offsets[0]);
+    leg_type spl = (fnl[fs_index].l_r == RLEG) ? LLEG :RLEG;
+    dz0 = _support_leg_coords.rot * ((spl == RLEG)? default_zmp_offsets[0] : default_zmp_offsets[1]);
+    dz1 = _swing_leg_coords.rot * ((spl == RLEG)? default_zmp_offsets[1] : default_zmp_offsets[0]);
     dz0 += _support_leg_coords.pos;
     dz1 += _swing_leg_coords.pos;
     rzmp = (dz0 + dz1) / 2.0;
@@ -30,7 +30,7 @@ namespace rats
   {
     hrp::Vector3 rzmp;
     coordinates tmp(fnl[fs_index-1].worldcoords);
-    rzmp = tmp.rot * ((fnl[fs_index-1].l_r == WC_RLEG)? default_zmp_offsets[0] : default_zmp_offsets[1]) + tmp.pos;
+    rzmp = tmp.rot * ((fnl[fs_index-1].l_r == RLEG)? default_zmp_offsets[0] : default_zmp_offsets[1]) + tmp.pos;
     refzmp_cur_list.push_back( rzmp );
     if (fs_index < fnl.size()) fs_index++;
   };
@@ -107,9 +107,9 @@ namespace rats
       ret = tmp_ratio;
       tmp_current_swing_time = current_swing_len * _dt;
     }
-    current_swing_time[support_leg==WC_RLEG?0:1] = (gp_count + 0.5 * default_double_support_ratio * one_step_len) * _dt;
-    current_swing_time[support_leg==WC_RLEG?1:0] = tmp_current_swing_time;
-    //std::cerr << "sl " << support_leg << " " << current_swing_time[support_leg==WC_RLEG?0:1] << " " << current_swing_time[support_leg==WC_RLEG?1:0] << " " << tmp_current_swing_time << " " << gp_count << std::endl;
+    current_swing_time[support_leg==RLEG?0:1] = (gp_count + 0.5 * default_double_support_ratio * one_step_len) * _dt;
+    current_swing_time[support_leg==RLEG?1:0] = tmp_current_swing_time;
+    //std::cerr << "sl " << support_leg << " " << current_swing_time[support_leg==RLEG?0:1] << " " << current_swing_time[support_leg==RLEG?1:0] << " " << tmp_current_swing_time << " " << gp_count << std::endl;
     return ret;
   };
 
@@ -355,26 +355,26 @@ namespace rats
       set_velocity_param(dp(0)/default_step_time, dp(1)/default_step_time, rad2deg(dr(2))/default_step_time);
       append_footstep_list_velocity_mode();
       foot_midcoords = footstep_node_list.back().worldcoords;
-      foot_midcoords.pos += foot_midcoords.rot * hrp::Vector3(footstep_param.leg_default_translate_pos[(footstep_node_list.back().l_r == WC_RLEG) ? 0 : 1] * -1.0);
+      foot_midcoords.pos += foot_midcoords.rot * hrp::Vector3(footstep_param.leg_default_translate_pos[(footstep_node_list.back().l_r == RLEG) ? 0 : 1] * -1.0);
       foot_midcoords.difference(dp, dr, goal_foot_midcoords);
       dp = foot_midcoords.rot.transpose() * dp;
       dr = foot_midcoords.rot.transpose() * dr;
     }
 
     /* finalize */
-    append_go_pos_step_node(foot_midcoords, (footstep_node_list.back().l_r == WC_RLEG ? WC_LLEG : WC_RLEG));
-    append_go_pos_step_node(foot_midcoords, (footstep_node_list.back().l_r == WC_RLEG ? WC_LLEG : WC_RLEG));
+    append_go_pos_step_node(foot_midcoords, (footstep_node_list.back().l_r == RLEG ? LLEG : RLEG));
+    append_go_pos_step_node(foot_midcoords, (footstep_node_list.back().l_r == RLEG ? LLEG : RLEG));
   };
 
   void gait_generator::go_single_step_param_2_footstep_list (const double goal_x, const double goal_y, const double goal_z, const double goal_theta,
                                                              const std::string& tmp_swing_leg,
                                                              const coordinates& _support_leg_coords)
   {
-    leg_type _swing_leg = (tmp_swing_leg == "rleg") ? WC_RLEG : WC_LLEG;
-    step_node sn0((_swing_leg == WC_RLEG) ? WC_LLEG : WC_RLEG, _support_leg_coords, lcg.get_default_step_height());
+    leg_type _swing_leg = (tmp_swing_leg == "rleg") ? RLEG : LLEG;
+    step_node sn0((_swing_leg == RLEG) ? LLEG : RLEG, _support_leg_coords, lcg.get_default_step_height());
     footstep_node_list.push_back(sn0);
     step_node sn1(_swing_leg, _support_leg_coords, lcg.get_default_step_height());
-    hrp::Vector3 trs(2.0 * footstep_param.leg_default_translate_pos[(_swing_leg == WC_RLEG) ? 0 : 1] + hrp::Vector3(goal_x, goal_y, goal_z));
+    hrp::Vector3 trs(2.0 * footstep_param.leg_default_translate_pos[(_swing_leg == RLEG) ? 0 : 1] + hrp::Vector3(goal_x, goal_y, goal_z));
     sn1.worldcoords.pos += sn1.worldcoords.rot * trs;
     sn1.worldcoords.rotate(deg2rad(goal_theta), hrp::Vector3(0,0,1));
     footstep_node_list.push_back(sn1);
@@ -386,7 +386,7 @@ namespace rats
   {
     velocity_mode_flg = VEL_DOING;
     /* initialize */
-    leg_type current_leg = (vel_y > 0.0) ? WC_RLEG : WC_LLEG;
+    leg_type current_leg = (vel_y > 0.0) ? RLEG : LLEG;
     clear_footstep_node_list();
     set_velocity_param (vel_x, vel_y, vel_theta);
     append_go_pos_step_node(_foot_midcoords, current_leg);
@@ -403,7 +403,7 @@ namespace rats
   void gait_generator::calc_foot_midcoords_trans_vector_velocity_mode (coordinates& foot_midcoords, hrp::Vector3& trans, double& dth, const step_node& sn)
   {
     foot_midcoords = sn.worldcoords;
-    hrp::Vector3 tmpv(footstep_param.leg_default_translate_pos[(sn.l_r == WC_RLEG) ? 0 : 1] * -1.0);
+    hrp::Vector3 tmpv(footstep_param.leg_default_translate_pos[(sn.l_r == RLEG) ? 0 : 1] * -1.0);
     foot_midcoords.pos += foot_midcoords.rot * tmpv;
     double dx = vel_param.velocity_x + offset_vel_param.velocity_x, dy = vel_param.velocity_y + offset_vel_param.velocity_y;
     dth = vel_param.velocity_theta + offset_vel_param.velocity_theta;
@@ -419,14 +419,14 @@ namespace rats
     /* inside step limitation */
     if (use_inside_step_limitation) {
       if (vel_param.velocity_y > 0) {
-        if (sn.l_r == WC_LLEG) dy *= 0.5;
+        if (sn.l_r == LLEG) dy *= 0.5;
       } else {
-        if (sn.l_r == WC_RLEG) dy *= 0.5;
+        if (sn.l_r == RLEG) dy *= 0.5;
       }
       if (vel_param.velocity_theta > 0) {
-        if (sn.l_r == WC_LLEG) dth *= 0.5;
+        if (sn.l_r == LLEG) dth *= 0.5;
       } else {
-        if (sn.l_r == WC_RLEG) dth *= 0.5;
+        if (sn.l_r == RLEG) dth *= 0.5;
       }
     }
     trans = hrp::Vector3(dx * default_step_time, dy * default_step_time, 0);
@@ -442,7 +442,7 @@ namespace rats
 
     foot_midcoords.pos += foot_midcoords.rot * trans;
     foot_midcoords.rotate(dth, hrp::Vector3(0,0,1));
-    append_go_pos_step_node(foot_midcoords, (( footstep_node_list.back().l_r == WC_RLEG ) ? WC_LLEG : WC_RLEG));
+    append_go_pos_step_node(foot_midcoords, (( footstep_node_list.back().l_r == RLEG ) ? LLEG : RLEG));
   };
 
   void gait_generator::calc_next_coords_velocity_mode (std::vector<coordinates>& ret, const size_t idx)
@@ -458,7 +458,7 @@ namespace rats
         ret[i].pos += ret[i].rot * trans;
         ret[i].rotate(dth, hrp::Vector3(0,0,1));
       }
-      ret[i].pos += ret[i].rot * footstep_param.leg_default_translate_pos[(footstep_node_list[idx-1].l_r == WC_RLEG) ? (1 + i)%2 : i%2];
+      ret[i].pos += ret[i].rot * footstep_param.leg_default_translate_pos[(footstep_node_list[idx-1].l_r == RLEG) ? (1 + i)%2 : i%2];
     }
   };
 
