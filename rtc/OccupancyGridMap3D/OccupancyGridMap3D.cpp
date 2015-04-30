b96a4a156453a556d9680211e6414e703fa500b8
Fumio KANEHIRO <fkanehiro@gmail.com>
2015-05-01 00:10:05 +0900

    outputs an update signal when onActivate() is called

--- rtc/OccupancyGridMap3D/OccupancyGridMap3D.cpp
+++ rtc/OccupancyGridMap3D/OccupancyGridMap3D.cpp
@@ -162,17 +162,16 @@ RTC::ReturnCode_t OccupancyGridMap3D::onActivated(RTC::UniqueId ec_id)
   Guard guard(m_mutex);
   if (m_knownMapPath != ""){
       m_knownMap = new OcTree(m_cwd+m_knownMapPath);
-      m_updateOut.write();
   }
 
   if (m_initialMap != ""){
     // Working directories of threads which calls onInitialize() and onActivate() are different on MacOS
     // Assume path of initial map is given by a relative path to working directory of the thread which calls onInitialize()
     m_map = new OcTree(m_cwd+m_initialMap);
-    m_updateOut.write();
   }else{
     m_map = new OcTree(m_resolution);
   }
+  m_updateOut.write();
 
   if(KDEBUG){
     std::cout << m_profile.instance_name << ": initial tree depth = " << m_map->getTreeDepth() << std::endl;
