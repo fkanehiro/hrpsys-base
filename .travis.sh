30fa64b04d147e773190587b85ec27ee85df90b2
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2015-05-02 08:57:05 +0900

    [.travis.sh] any diff between 315.1.9 and current is not permitted, since we use cproto without any -DROBOT_IOB_VERSION, so this should output header file compatible with stable version

--- .travis.sh
+++ .travis.sh
@@ -94,7 +94,7 @@ case $TEST_PACKAGE in
                 echo -e "#define pid_t int\n#define size_t int\n#include \"iob.h.315.1.9\"" | cproto -x - | sort > iob.h.stable
                 cat iob.h.current
                 cat iob.h.stable
-                diff iob.h.stable iob.h.current | tee >(cat - 1>&2)  | diffstat | grep -c deletion && exit 1
+                diff iob.h.stable iob.h.current || exit 1
 
                 travis_time_end
                 ;;
