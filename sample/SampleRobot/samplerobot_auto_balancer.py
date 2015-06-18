575d0ea4b63646b7138b27b75e2ddac0f2298319
Shunichi Nozawa <notheworld@gmail.com>
2015-06-18 21:50:14 +0900

    Use functions defined in hrpsys_config.py

--- sample/SampleRobot/samplerobot_auto_balancer.py
+++ sample/SampleRobot/samplerobot_auto_balancer.py
@@ -26,33 +26,33 @@ def init ():
     root_rot_y_pose = [8.251963e-05,-0.980029,-0.000384,1.02994,-0.398115,-0.000111,0.31129,-0.159481,-0.115399,-0.636277,0.0,0.0,0.0,8.252625e-05,-0.980033,-0.000384,1.02986,-0.398027,-0.000111,0.31129,0.159481,0.115399,-0.636277,0.0,0.0,0.0,0.0,0.0,0.0]
     pose_list=[half_sitting_pose, root_rot_x_pose, root_rot_y_pose, arm_front_pose]
     hcf.seq_svc.setJointAngles(initial_pose, 2.0)
-    hcf.seq_svc.waitInterpolation()
+    hcf.waitInterpolation()
 
 def testPoseList(pose_list, initial_pose):
     for pose in pose_list:
         hcf.seq_svc.setJointAngles(pose, 1.0)
-        hcf.seq_svc.waitInterpolation()
+        hcf.waitInterpolation()
         hcf.seq_svc.setJointAngles(initial_pose, 1.0)
-        hcf.seq_svc.waitInterpolation()
+        hcf.waitInterpolation()
 
 def demoAutoBalancerFixFeet ():
     print "1. AutoBalancer mode by fixing feet"
-    hcf.abc_svc.startAutoBalancer(["rleg", "lleg"]);
+    hcf.startAutoBalancer();
     hcf.seq_svc.setJointAngles(arm_front_pose, 1.0)
-    hcf.seq_svc.waitInterpolation()
+    hcf.waitInterpolation()
     hcf.seq_svc.setJointAngles(initial_pose, 1.0)
-    hcf.seq_svc.waitInterpolation()
-    hcf.abc_svc.stopAutoBalancer();
+    hcf.waitInterpolation()
+    hcf.stopAutoBalancer();
     print "  Start and Stop AutoBalancer by fixing feet=>OK"
 
 def demoAutoBalancerFixFeetHands ():
     print "2. AutoBalancer mode by fixing hands and feet"
-    hcf.abc_svc.startAutoBalancer(["rleg", "lleg", "rarm", "larm"])
+    hcf.startAutoBalancer(["rleg", "lleg", "rarm", "larm"])
     hcf.seq_svc.setJointAngles(arm_front_pose, 1.0)
-    hcf.seq_svc.waitInterpolation()
+    hcf.waitInterpolation()
     hcf.seq_svc.setJointAngles(initial_pose, 1.0)
-    hcf.seq_svc.waitInterpolation()
-    hcf.abc_svc.stopAutoBalancer();
+    hcf.waitInterpolation()
+    hcf.stopAutoBalancer();
     print "  Start and Stop AutoBalancer by fixing hands and feet=>OK"
 
 def demoAutoBalancerGetParam():
@@ -70,49 +70,49 @@ def demoAutoBalancerSetParam():
     if ret[0] and ret[1].default_zmp_offsets == abcp.default_zmp_offsets:
         print "  setAutoBalancerParam() => OK"
     print "  default_zmp_offsets setting check in start and stop"
-    hcf.abc_svc.startAutoBalancer(["rleg", "lleg"]);
-    hcf.abc_svc.stopAutoBalancer();
+    hcf.startAutoBalancer();
+    hcf.stopAutoBalancer();
     abcp.default_zmp_offsets = [[0,0,0], [0,0,0]]
     hcf.abc_svc.setAutoBalancerParam(abcp)
 
 def demoAutoBalancerTestPoses():
     print "5. change base height, base rot x, base rot y, and upper body while AutoBalancer mode"
-    hcf.abc_svc.startAutoBalancer(["rleg", "lleg"]);
+    hcf.startAutoBalancer();
     testPoseList(pose_list, initial_pose)
-    hcf.abc_svc.stopAutoBalancer();
+    hcf.stopAutoBalancer();
 
 def demoAutoBalancerStartStopCheck():
     print "6. start stop check"
     abcp=hcf.abc_svc.getAutoBalancerParam()[1]
     abcp.default_zmp_offsets = [[-0.05,0.05,0], [-0.05,0.05,0]]
     hcf.abc_svc.setAutoBalancerParam(abcp)
-    hcf.log_svc.maxLength(1500)
+    hcf.setMaxLogLength(1500)
     for pose in pose_list:
         hcf.seq_svc.setJointAngles(pose, 1.0)
-        hcf.seq_svc.waitInterpolation()
-        hcf.log_svc.clear()
-        hcf.abc_svc.startAutoBalancer(["rleg", "lleg"]);
-        hcf.abc_svc.stopAutoBalancer();
-        hcf.log_svc.save("/tmp/test-samplerobot-abc-startstop-{0}".format(pose_list.index(pose)))
+        hcf.waitInterpolation()
+        hcf.clearLog()
+        hcf.startAutoBalancer();
+        hcf.stopAutoBalancer();
+        hcf.saveLog("/tmp/test-samplerobot-abc-startstop-{0}".format(pose_list.index(pose)))
     abcp.default_zmp_offsets = [[0,0,0], [0,0,0]]
     hcf.abc_svc.setAutoBalancerParam(abcp)
     hcf.seq_svc.setJointAngles(initial_pose, 1.0)
-    hcf.seq_svc.waitInterpolation()
+    hcf.waitInterpolation()
 
 def demoAutoBalancerBalanceAgainstHandForce():
     print "7. balance against hand force"
-    hcf.abc_svc.startAutoBalancer(["rleg", "lleg"]);
+    hcf.startAutoBalancer();
     hcf.seq_svc.setWrenches([0,0,0,0,0,0,
                              0,0,0,0,0,0,
                              0,0,0,0,0,0,
                              0,0,-50,0,0,0,], 1.0); # rhsensor
-    hcf.seq_svc.waitInterpolation();
+    hcf.waitInterpolation();
     hcf.seq_svc.setWrenches([0,0,0,0,0,0,
                              0,0,0,0,0,0,
                              0,0,0,0,0,0,
                              0,0,0,0,0,0,], 1.0);
-    hcf.seq_svc.waitInterpolation();
-    hcf.abc_svc.stopAutoBalancer();
+    hcf.waitInterpolation();
+    hcf.stopAutoBalancer();
 
 def demoGaitGeneratorGoPos():
     print "1. goPos"
@@ -141,12 +141,12 @@ def demoGaitGeneratorSetFootSteps():
 
 def demoGaitGeneratorChangePoseWhileWalking():
     print "4. Change base height, base rot x, base rot y, and upper body while walking"
-    hcf.abc_svc.startAutoBalancer(["rleg", "lleg"]);
+    hcf.startAutoBalancer();
     hcf.abc_svc.waitFootSteps()
     hcf.abc_svc.goVelocity(0,0,0)
     testPoseList(pose_list, initial_pose)
     hcf.abc_svc.goStop()
-    hcf.abc_svc.stopAutoBalancer();
+    hcf.stopAutoBalancer();
 
 def demoGaitGeneratorGetParam():
     print "5. getGaitGeneratorParam"
@@ -171,19 +171,19 @@ def demoGaitGeneratorSetParam():
 
 def demoGaitGeneratorNonDefaultStrideStop():
     print "7. non-default stride"
-    hcf.abc_svc.startAutoBalancer(["rleg", "lleg"]);
+    hcf.startAutoBalancer();
     hcf.abc_svc.setFootSteps([OpenHRP.AutoBalancerService.Footstep([0,-0.09,0], [1,0,0,0], "rleg"),
                               OpenHRP.AutoBalancerService.Footstep([0.15,0.09,0], [1,0,0,0], "lleg")])
     hcf.abc_svc.waitFootSteps()
     hcf.abc_svc.setFootSteps([OpenHRP.AutoBalancerService.Footstep([0,-0.09,0], [1,0,0,0], "rleg"),
                               OpenHRP.AutoBalancerService.Footstep([0,0.09,0], [1,0,0,0], "lleg")])
     hcf.abc_svc.waitFootSteps()
-    hcf.abc_svc.stopAutoBalancer();
+    hcf.stopAutoBalancer();
     print "  Non default Stride()=>OK"
 
 def demoGaitGeneratorToeHeelContact():
     print "8. Use toe heel contact"
-    hcf.abc_svc.startAutoBalancer(["rleg", "lleg"]);
+    hcf.startAutoBalancer();
     ggp=hcf.abc_svc.getGaitGeneratorParam()[1];
     ggp.toe_pos_offset_x = 1e-3*182.0;
     ggp.heel_pos_offset_x = 1e-3*-72.0;
@@ -194,7 +194,7 @@ def demoGaitGeneratorToeHeelContact():
     hcf.abc_svc.setGaitGeneratorParam(ggp);
     hcf.abc_svc.goPos(0.3, 0, 0);
     hcf.abc_svc.waitFootSteps()
-    hcf.abc_svc.stopAutoBalancer();
+    hcf.stopAutoBalancer();
     ggp.toe_angle = 0;
     ggp.heel_angle = 0;
     hcf.abc_svc.setGaitGeneratorParam(ggp);
@@ -211,7 +211,7 @@ def demoGaitGeneratorStopStartSyncCheck():
     print "  Check 9-2 Sync from setJointAngles at the beginning"
     open_stride_pose = [0.00026722677758058496, -0.3170503560247552, -0.0002054613599000865, 0.8240549352035262, -0.5061434785447525, -8.67443660992421e-05, 0.3112899999999996, -0.15948099999999998, -0.11539900000000003, -0.6362769999999993, 0.0, 0.0, 0.0, 0.00023087433689200683, -0.4751295978345554, -0.00021953834197007937, 0.8048588066686679, -0.3288687069275527, -8.676469399681631e-05, 0.3112899999999996, 0.15948099999999998, 0.11539900000000003, -0.6362769999999993, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     hcf.seq_svc.setJointAngles(open_stride_pose, 2.0);
-    hcf.seq_svc.waitInterpolation();
+    hcf.waitInterpolation();
     hcf.startAutoBalancer();
     hcf.abc_svc.setFootSteps([OpenHRP.AutoBalancerService.Footstep([0,-0.09,0], [1,0,0,0], "rleg"), OpenHRP.AutoBalancerService.Footstep([0.1,0.09,0], [1,0,0,0], "lleg")]);
     hcf.abc_svc.waitFootSteps();
@@ -220,7 +220,7 @@ def demoGaitGeneratorStopStartSyncCheck():
     print "  Check 9-3 Sync from setJointAngles"
     hcf.startAutoBalancer();
     hcf.seq_svc.setJointAngles(initial_pose, 2.0);
-    hcf.seq_svc.waitInterpolation();
+    hcf.waitInterpolation();
     hcf.stopAutoBalancer();
     print "    Sync from setJointAngle => OK"
 
@@ -232,7 +232,7 @@ def demoGaitGeneratorEmergencyStop():
     for idx in range(4): # Wait for 4 steps including initial double support phase
         # Wait for 1 steps
         hcf.seq_svc.setJointAngles(initial_pose, hcf.abc_svc.getGaitGeneratorParam()[1].default_step_time);
-        hcf.seq_svc.waitInterpolation();
+        hcf.waitInterpolation();
     print "  Emergency stoping"
     hcf.abc_svc.emergencyStop();
     print "  Align foot steps"
@@ -248,7 +248,7 @@ def demoGaitGeneratorGetRemainingSteps():
         print "  Remaining footstep ", len(fslist)
         # Wait for 1 step
         hcf.seq_svc.setJointAngles(initial_pose, hcf.abc_svc.getGaitGeneratorParam()[1].default_step_time);
-        hcf.seq_svc.waitInterpolation();
+        hcf.waitInterpolation();
 
 def demoGaitGeneratorHandFixWalking():
     print "12. walking by fixing"
@@ -262,7 +262,6 @@ def demoGaitGeneratorHandFixWalking():
 
 def demo():
     init()
-    return
 
     # sample for AutoBalancer mode
     demoAutoBalancerFixFeet()
