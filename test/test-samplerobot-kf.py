e2a982c2c268983ab2d2d8bee15a563c3514c113
Shunichi Nozawa <notheworld@gmail.com>
2015-07-21 02:23:11 +0900

    [test/test-samplerobot.test, test-samplerobot-*.py] Add rostests for unstable rtcs.

---
+++ test/test-samplerobot-kf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import imp, sys, os, time
+
+# set path to hrpsys to use HrpsysConfigurator
+from subprocess import check_output
+sys.path.append(os.path.join(check_output(['pkg-config', 'hrpsys-base', '--variable=prefix']).rstrip(),'share/hrpsys/samples/SampleRobot/')) # set path to SampleRobot
+
+import samplerobot_kalman_filter
+
+if __name__ == '__main__':
+    samplerobot_kalman_filter.demo()
+
+## IGNORE ME: this code used for rostest
+if [s for s in sys.argv if "--gtest_output=xml:" in s] :
+    import unittest, rostest
+    rostest.run('hrpsys', 'samplerobot_kalman_filter', unittest.TestCase, sys.argv)
+
+
+
+
+
+
