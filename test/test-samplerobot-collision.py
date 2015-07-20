daf056af67cdf4ed587ab01114423eea2a0347ff
Shunichi Nozawa <notheworld@gmail.com>
2015-07-20 18:50:56 +0900

    [test/test-samplerobot-collision.py,test-samplerobot-datalogger.py] Add unittest for collision and datalogger

---
+++ test/test-samplerobot-collision.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import imp, sys, os, time
+
+# set path to hrpsys to use HrpsysConfigurator
+from subprocess import check_output
+sys.path.append(os.path.join(check_output(['pkg-config', 'hrpsys-base', '--variable=prefix']).rstrip(),'share/hrpsys/samples/SampleRobot/')) # set path to SampleRobot
+
+import samplerobot_collision_detector
+
+if __name__ == '__main__':
+    samplerobot_collision_detector.demo()
+
+## IGNORE ME: this code used for rostest
+if [s for s in sys.argv if "--gtest_output=xml:" in s] :
+    import unittest, rostest
+    rostest.run('hrpsys', 'samplerobot_collision_detector', unittest.TestCase, sys.argv)
+
+
+
+
+
+
