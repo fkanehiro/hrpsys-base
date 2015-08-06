4cbced785155982ed8fb48c3fe2d9f7877e167ae
eisoku9618 <kuroiwa@jsk.imi.i.u-tokyo.ac.jp>
2015-08-06 21:24:20 +0900

    [GaitGenerator.h] add hints for the second template argument of boost::assign::list_of at a constructor initialization phase

--- rtc/AutoBalancer/GaitGenerator.h
+++ rtc/AutoBalancer/GaitGenerator.h
@@ -807,8 +807,8 @@ namespace rats
         velocity_mode_flg(VEL_IDLING), emergency_flg(IDLING),
         use_inside_step_limitation(true),
         preview_controller_ptr(NULL) {
-        swing_foot_zmp_offsets = boost::assign::list_of(hrp::Vector3::Zero());
-        prev_que_sfzos = boost::assign::list_of(hrp::Vector3::Zero());
+        swing_foot_zmp_offsets = boost::assign::list_of<hrp::Vector3>(hrp::Vector3::Zero());
+        prev_que_sfzos = boost::assign::list_of<hrp::Vector3>(hrp::Vector3::Zero());
     };
     ~gait_generator () {
       if ( preview_controller_ptr != NULL ) {
