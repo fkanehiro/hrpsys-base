5b1c32517074221f2deee04e752a4bdba28bbad0
YutaKojio <kojio@jsk.imi.i.u-tokyo.ac.jp>
2015-11-26 11:07:18 +0900

    [idl/StabilizerService.idl, rtc/Stabilizer/ZMPDistributor.h, rtc/Stabilizer/Stabilizer.*] change variable type of cp_check_margin

--- rtc/Stabilizer/Stabilizer.h
+++ rtc/Stabilizer/Stabilizer.h
@@ -291,7 +291,7 @@ class Stabilizer
   hrp::Vector3 new_refzmp, rel_cog, ref_zmp_aux;
   hrp::Vector3 pos_ctrl;
   double total_mass, transition_time, cop_check_margin, contact_decision_threshold;
-  hrp::Vector3  cp_check_margin;
+  std::vector<double> cp_check_margin;
   OpenHRP::StabilizerService::EmergencyCheckMode emergency_check_mode;
 };
 
