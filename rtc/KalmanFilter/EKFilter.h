f700c9391bf54a084f86515a3c346f1a79c56064
eisoku9618 <kuroiwa@jsk.imi.i.u-tokyo.ac.jp>
2016-02-25 02:46:58 +0900

    [KalmanFilter/EKFilter.h] use fuzzy logic to tune R matrix

--- rtc/KalmanFilter/EKFilter.h
+++ rtc/KalmanFilter/EKFilter.h
@@ -18,7 +18,9 @@ public:
       Q(Eigen::Matrix3d::Identity() * 0.001),
       R(Eigen::Matrix3d::Identity() * 0.03),
       g_vec(Eigen::Vector3d(0.0, 0.0, 9.80665)),
-      z_k(Eigen::Vector3d(0.0, 0.0, 9.80665))
+      z_k(Eigen::Vector3d(0.0, 0.0, 9.80665)),
+      min_mag_thre_acc(0.005), max_mag_thre_acc(0.05),
+      min_mag_thre_gyro(0.0075), max_mag_thre_gyro(0.035)
   {
     x << 1, 0, 0, 0, 0, 0, 0;
   }
@@ -117,13 +119,13 @@ public:
     P_a_priori = P_tmp;
   }
 
-  void correction(const Eigen::Vector3d& z) {
+  void correction(const Eigen::Vector3d& z, const Eigen::Matrix3d& fussyR) {
     Eigen::Vector4d q_a_priori = x_a_priori.head<4>();
     Eigen::Matrix<double, 3, 7> H;
     z_k = z;
     Eigen::Vector3d y = calcMeasurementResidual(z, q_a_priori);
     calcH(H, q_a_priori);
-    Eigen::Matrix3d S = H * P_a_priori * H.transpose() + R;
+    Eigen::Matrix3d S = H * P_a_priori * H.transpose() + fussyR;
     Eigen::Matrix<double, 7, 3> K = P_a_priori * H.transpose() * S.inverse();
     hrp::Vector7 x_tmp = x_a_priori + K * y;
     x.head<4>() = x_tmp.head<4>().normalized(); /* quaternion */
@@ -138,10 +140,31 @@ public:
     std::cerr << "P_a_priori" << std::endl << P_a_priori << std::endl << std::endl;
   }
 
+
+  // Basically Equation (23), (24) and (25) in the paper [1]
+  // [1] Chul Woo Kang and Chan Gook Park. Attitude estimation with accelerometers and gyros using fuzzy tuned Kalman filter.
+  //     In European Control Conference, 2009.
+  void calcRWithFuzzyRule(Eigen::Matrix3d& fussyR, const hrp::Vector3& acc, const hrp::Vector3& gyro) const {
+    double alpha = std::min(std::fabs(acc.norm() - g_vec.norm()) / g_vec.norm(), 0.1);
+    double beta = std::min(gyro.norm(), 0.05);
+    double large_mu_acc = std::max(std::min((alpha - min_mag_thre_acc) / (max_mag_thre_acc - min_mag_thre_acc), 1.0), 0.0);
+    double large_mu_gyro = std::max(std::min((beta - min_mag_thre_gyro) / (max_mag_thre_gyro - min_mag_thre_gyro), 1.0), 0.0);
+    double w1, w2, w3, w4;
+    w1 = (1.0 - large_mu_acc) * (1.0 - large_mu_gyro);
+    w2 = (1.0 - large_mu_acc) * large_mu_gyro;
+    w3 = large_mu_acc * (1.0 - large_mu_gyro);
+    w4 = large_mu_acc * large_mu_gyro;
+    double z = (w1 * 0.0 + w2 * (3.5 * alpha + 8.0 * beta + 0.5) + w3 * (3.5 * alpha + 8.0 * beta + 0.5) + w4 * 1.0) / (w1 + w2 + w3 + w4);
+    double k1 = 400;
+    fussyR = R + k1 * z * z * Eigen::Matrix3d::Identity();
+  };
+
   void main_one (hrp::Vector3& rpy, hrp::Vector3& rpyRaw, const hrp::Vector3& acc, const hrp::Vector3& gyro)
   {
+    Eigen::Matrix3d fussyR;
+    calcRWithFuzzyRule(fussyR, acc, gyro);
     prediction(gyro);
-    correction(acc);
+    correction(acc, fussyR);
     /* ekf_filter.printAll(); */
     Eigen::Quaternion<double> q(x[0], x[1], x[2], x[3]);
     rpy = hrp::rpyFromRot(q.toRotationMatrix());
@@ -159,6 +182,7 @@ private:
   Eigen::Matrix3d Q, R;
   Eigen::Vector3d g_vec, z_k;
   double dt;
+  double min_mag_thre_acc, max_mag_thre_acc, min_mag_thre_gyro, max_mag_thre_gyro;
 };
 
 #endif /* EKFILTER_H */
