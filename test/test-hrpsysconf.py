de816f6a371bdfe506dc44274ec890f570334ad7
Kei Okada <kei.okada@gmail.com>
2016-04-15 16:42:23 +0900

    test/test-hrpsysconf.py: add check do not connect again if already connected for https://github.com/fkanehiro/hrpsys-base/issues/979

--- test/test-hrpsysconf.py
+++ test/test-hrpsysconf.py
@@ -23,11 +23,28 @@ class SampleHrpsysConfigurator(HrpsysConfigurator):
 
 class TestHrpsysConfig(unittest.TestCase):
     global h
+    rh = None
+    seq = None
 
     def test_import_waitinput(self):
         # https://github.com/start-jsk/rtmros_hironx/blob/groovy-devel/hironx_ros_bridge/src/hironx_ros_bridge/hironx_client.py
         from waitInput import waitInputConfirm, waitInputSelect
         self.assertTrue(True)
+
+    def test_createcomp(self):
+        global h
+        self.seq = h.createComp("SequencePlayer",'seq')[0]
+
+    def test_connectcomp(self):
+        global h
+        if self.seq == None or self.rh == None:
+            self.test_createcomp()
+        connectPorts(self.rh.port("q"), self.seq.port("qInit"))
+        # check number of connection
+        assert(len(self.seq.port("qInit").get_connector_profiles()) == 1)
+        # check do not connect again if already connected for https://github.com/fkanehiro/hrpsys-base/issues/979
+        connectPorts(self.rh.port("q"), self.seq.port("qInit"))
+        assert(len(self.seq.port("qInit").get_connector_profiles()) == 1)
 
     def test_findcomp(self):
         global h
@@ -46,6 +63,13 @@ class TestHrpsysConfig(unittest.TestCase):
             rtm.nsport = args.port
         h = SampleHrpsysConfigurator()
 
+        h.waitForRTCManager()
+        # look for name
+        for c in h.ms.get_components():
+            if '(Robot)' in c.name() or 'RobotHardware' in c.name():
+                h.waitForRobotHardware(c.name())  # get robot hardware name
+                break;
+        self.rh = h.rh
 
 #unittest.main()
 if __name__ == '__main__':
