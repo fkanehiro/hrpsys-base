f7d8bcd70e15befd5cc528711bc1baa19678ad8d
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2016-06-21 17:10:51 +0900

    [CollisionDetector] fix, get out from initial collision state

--- rtc/CollisionDetector/CollisionDetector.cpp
+++ rtc/CollisionDetector/CollisionDetector.cpp
@@ -452,6 +452,14 @@ RTC::ReturnCode_t CollisionDetector::onExecute(RTC::UniqueId ec_id)
                 }
             }
             if ( m_safe_posture ) {
+                if (! m_have_safe_posture ) {
+                    // first transition collision -> safe
+                    std::cerr << "[" << m_profile.instance_name << "] set safe posture" << std::endl;
+                    for ( int i = 0; i < m_q.data.length(); i++ ) {
+                        m_lastsafe_jointdata[i] = m_robot->joint(i)->q;
+                    }
+                    m_interpolator->set(m_lastsafe_jointdata); // Set current angles as initial angle for recover
+                }
                 m_have_safe_posture = true;
                 if (m_recover_time != default_recover_time) {
                     // sefe or recover
