4aaae1d4b52c2b46d0ab45b070c067582e1ef80e
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2016-10-05 11:49:57 +0900

    [IIRFilter] fix indent on IIRFilter.h IIRFilter.cpp

--- rtc/TorqueFilter/IIRFilter.cpp
+++ rtc/TorqueFilter/IIRFilter.cpp
@@ -2,83 +2,83 @@
 
 IIRFilter::IIRFilter(unsigned int dim, std::vector<double>& fb_coeffs, std::vector<double>& ff_coeffs, const std::string& error_prefix)
 {
-  std::cerr << "This IIRFilter constructure is obsolated method." << std::endl;
-  m_dimension = dim;
-  m_error_prefix = error_prefix;
+    std::cerr << "This IIRFilter constructure is obsolated method." << std::endl;
+    m_dimension = dim;
+    m_error_prefix = error_prefix;
 
-  // init coefficients
-  if(fb_coeffs.size() != dim + 1|| ff_coeffs.size() != dim + 1){
-    std::cout << "[" <<  m_error_prefix << "]" << "IIRFilter coefficients size error" << std::endl;
+    // init coefficients
+    if(fb_coeffs.size() != dim + 1|| ff_coeffs.size() != dim + 1){
+        std::cout << "[" <<  m_error_prefix << "]" << "IIRFilter coefficients size error" << std::endl;
+        return;
+    }
+    for(std::vector<double>::iterator it = fb_coeffs.begin(); it != fb_coeffs.end(); it++){
+        m_fb_coefficients.push_back(*it);
+    }
+    for(std::vector<double>::iterator it = ff_coeffs.begin(); it != ff_coeffs.end(); it++){
+        m_ff_coefficients.push_back(*it);
+    }
+
+    // init previous values
+    m_previous_values.assign(dim, 0.0);
+    m_initialized = true;
     return;
-  }
-  for(std::vector<double>::iterator it = fb_coeffs.begin(); it != fb_coeffs.end(); it++){
-    m_fb_coefficients.push_back(*it);
-  }
-  for(std::vector<double>::iterator it = ff_coeffs.begin(); it != ff_coeffs.end(); it++){
-    m_ff_coefficients.push_back(*it);
-  }
-  
-  // init previous values
-  m_previous_values.assign(dim, 0.0);
-  m_initialized = true;
-  return;
 }
 
 IIRFilter::IIRFilter(const std::string& error_prefix) :
-  m_initialized(false) {
-  m_error_prefix = error_prefix;
+    m_initialized(false) {
+    m_error_prefix = error_prefix;
 }
 
 bool IIRFilter::setParameter(int dim, std::vector<double>& A, std::vector<double> B) {
-  m_dimension = dim;
+    m_dimension = dim;
 
-  // init coefficients
-  if((A.size() != dim && A.size() != dim + 1) || B.size() != dim + 1) {
-    std::cout << "[" <<  m_error_prefix << "]" << "IIRFilter coefficients size error" << std::endl;
-    return false;
-  }
-
-  if (A.size() == dim) {
-    m_fb_coefficients.push_back(1.0);
-  }
-  for(std::vector<double>::iterator it = A.begin(); it != A.end(); it++){
-    if (it == A.begin()) {
-      if( *it != 1.0 ) {
-        std::cout << "[" <<  m_error_prefix << "]" << "IIRFilter : parameter A[0] is not 1.0 !!!" << std::endl;
-      }
-      m_fb_coefficients.push_back(*it);
-    } else {
-      m_fb_coefficients.push_back(- *it);
+    // init coefficients
+    if((A.size() != dim && A.size() != dim + 1) || B.size() != dim + 1) {
+        std::cout << "[" <<  m_error_prefix << "]" << "IIRFilter coefficients size error" << std::endl;
+        return false;
     }
-  }
-  for(std::vector<double>::iterator it = B.begin(); it != B.end(); it++){
-    m_ff_coefficients.push_back(*it);
-  }
 
-  // init previous values
-  m_previous_values.assign(dim, 0.0);
-  m_initialized = true;
-  return true;
+    if (A.size() == dim) {
+        m_fb_coefficients.push_back(1.0);
+    }
+    for(std::vector<double>::iterator it = A.begin(); it != A.end(); it++){
+        if (it == A.begin()) {
+            if( *it != 1.0 ) {
+                std::cout << "[" <<  m_error_prefix << "]" << "IIRFilter : parameter A[0] is not 1.0 !!!" << std::endl;
+            }
+            m_fb_coefficients.push_back(*it);
+        } else {
+            m_fb_coefficients.push_back(- *it);
+        }
+    }
+    for(std::vector<double>::iterator it = B.begin(); it != B.end(); it++){
+        m_ff_coefficients.push_back(*it);
+    }
+
+    // init previous values
+    m_previous_values.assign(dim, 0.0);
+    m_initialized = true;
+    return true;
 }
 
 double IIRFilter::passFilter(double input)
 {
-  if (! m_initialized) {
-    return 0.0;
-  }
-  double feedback, filtered;
-  // calcurate retval
-  feedback = m_fb_coefficients[0] * input;
-  for (int i = 0; i < m_dimension; i++) {
-    feedback += m_fb_coefficients[i + 1] * m_previous_values[i];
-  }
-  filtered = m_ff_coefficients[0] * feedback;
-  for(int i = 0; i < m_dimension; i++) {
-    filtered += m_ff_coefficients[i + 1] * m_previous_values[i];
-  }
-  // update previous values
-  m_previous_values.push_front(feedback);
-  m_previous_values.pop_back();
+    if (! m_initialized) {
+        return 0.0;
+    }
+    double feedback, filtered;
+    // calcurate retval
+    feedback = m_fb_coefficients[0] * input;
+    for (int i = 0; i < m_dimension; i++) {
+        feedback += m_fb_coefficients[i + 1] * m_previous_values[i];
+    }
+    filtered = m_ff_coefficients[0] * feedback;
+    for(int i = 0; i < m_dimension; i++) {
+        filtered += m_ff_coefficients[i + 1] * m_previous_values[i];
+    }
+    // update previous values
+    m_previous_values.push_front(feedback);
+    m_previous_values.pop_back();
 
-  return filtered;
+    return filtered;
 }
