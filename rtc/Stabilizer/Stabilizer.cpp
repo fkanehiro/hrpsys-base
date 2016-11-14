702a235597808e70a93bd12028ae895147316fe6
Yuta Kojio <kojio@jsk.imi.i.u-tokyo.ac.jp>
2016-11-14 17:31:31 +0900

    [Stabilizer.cpp] smoothly change root height when switching use_limb_stretch_avoidance

--- rtc/Stabilizer/Stabilizer.cpp
+++ rtc/Stabilizer/Stabilizer.cpp
@@ -620,6 +620,7 @@ RTC::ReturnCode_t Stabilizer::onExecute(RTC::UniqueId ec_id)
       control_mode = MODE_AIR;
       break;
     }
+    if (control_mode != MODE_ST) d_pos_z_root = 0.0;
   }
   if ( m_robot->numJoints() == m_qRef.data.length() ) {
     if (is_legged_robot) {
@@ -1510,7 +1511,6 @@ void Stabilizer::calcEEForceMomentControl() {
       //   IK target is link origin pos and rot, not ee pos and rot.
       std::vector<hrp::Vector3> tmpp(stikp.size());
       std::vector<hrp::Matrix33> tmpR(stikp.size());
-      double tmp_d_pos_z_root = 0.0;
       for (size_t i = 0; i < stikp.size(); i++) {
         if (is_ik_enable[i]) {
           // Add damping_control compensation to target value
@@ -1530,7 +1530,7 @@ void Stabilizer::calcEEForceMomentControl() {
         }
       }
 
-      if (use_limb_stretch_avoidance) limbStretchAvoidanceControl(tmpp ,tmpR);
+      limbStretchAvoidanceControl(tmpp ,tmpR);
 
       // IK
       for (size_t i = 0; i < stikp.size(); i++) {
@@ -1594,23 +1594,26 @@ void Stabilizer::calcSwingEEModification ()
 
 void Stabilizer::limbStretchAvoidanceControl (const std::vector<hrp::Vector3>& ee_p, const std::vector<hrp::Matrix33>& ee_R)
 {
-  double tmp_d_pos_z_root = 0.0;
-  for (size_t i = 0; i < stikp.size(); i++) {
-    if (is_ik_enable[i]) {
-      // Check whether inside limb length limitation
-      hrp::Link* parent_link = m_robot->link(stikp[i].parent_name);
-      hrp::Vector3 targetp = (ee_p[i] - ee_R[i] * stikp[i].localR.transpose() * stikp[i].localp) - parent_link->p; // position from parent to target link (world frame)
-      double limb_length_limitation = stikp[i].max_limb_length - stikp[i].limb_length_margin;
-      double tmp = limb_length_limitation * limb_length_limitation - targetp(0) * targetp(0) - targetp(1) * targetp(1);
-      if (targetp.norm() > limb_length_limitation && tmp >= 0) {
-        tmp_d_pos_z_root = std::min(tmp_d_pos_z_root, targetp(2) + std::sqrt(tmp));
+  double tmp_d_pos_z_root = 0.0, prev_d_pos_z_root = d_pos_z_root;
+  if (use_limb_stretch_avoidance) {
+    for (size_t i = 0; i < stikp.size(); i++) {
+      if (is_ik_enable[i]) {
+        // Check whether inside limb length limitation
+        hrp::Link* parent_link = m_robot->link(stikp[i].parent_name);
+        hrp::Vector3 targetp = (ee_p[i] - ee_R[i] * stikp[i].localR.transpose() * stikp[i].localp) - parent_link->p; // position from parent to target link (world frame)
+        double limb_length_limitation = stikp[i].max_limb_length - stikp[i].limb_length_margin;
+        double tmp = limb_length_limitation * limb_length_limitation - targetp(0) * targetp(0) - targetp(1) * targetp(1);
+        if (targetp.norm() > limb_length_limitation && tmp >= 0) {
+          tmp_d_pos_z_root = std::min(tmp_d_pos_z_root, targetp(2) + std::sqrt(tmp));
+        }
       }
     }
+    tmp_d_pos_z_root = std::min(tmp_d_pos_z_root, interpolated_d_pos_z_root);
+    // Change root link height depending on limb length
+    d_pos_z_root = tmp_d_pos_z_root == 0.0 ? calcDampingControl(d_pos_z_root, limb_stretch_avoidance_time_const) : tmp_d_pos_z_root;
+  } else {
+    d_pos_z_root = calcDampingControl(d_pos_z_root, limb_stretch_avoidance_time_const);
   }
-  tmp_d_pos_z_root = std::min(tmp_d_pos_z_root, interpolated_d_pos_z_root);
-  // Change root link height depending on limb length
-  double prev_d_pos_z_root = d_pos_z_root;
-  d_pos_z_root = tmp_d_pos_z_root == 0.0 ? calcDampingControl(d_pos_z_root, limb_stretch_avoidance_time_const) : tmp_d_pos_z_root;
   d_pos_z_root = vlimit(d_pos_z_root, prev_d_pos_z_root + limb_stretch_avoidance_vlimit[0], prev_d_pos_z_root + limb_stretch_avoidance_vlimit[1]);
   m_robot->rootLink()->p(2) += d_pos_z_root;
 }
