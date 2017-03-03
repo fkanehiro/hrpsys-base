bbfa4960bad030827b619ebc7351d4e38a28c00b
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2017-03-03 12:49:34 -0800

    [doc] Clarify workspace for get*Pose methods. [doc] Add missing rtype for setJoint* methods.

--- python/hrpsys_config.py
+++ python/hrpsys_config.py
@@ -1037,6 +1037,8 @@ class HrpsysConfigurator(object):
         @param jname str: name of joint
         @param angle float: In degree.
         @param tm float: Time to complete.
+        @rtype bool
+        @return False upon any problem during execution.
         '''
         radangle = angle / 180.0 * math.pi
         return self.seq_svc.setJointAngle(jname, radangle, tm)
@@ -1052,6 +1054,8 @@ class HrpsysConfigurator(object):
         \endverbatim
         @param angles list of float: In degree.
         @param tm float: Time to complete.
+        @rtype bool
+        @return False upon any problem during execution.
         '''
         ret = []
         for angle in angles:
@@ -1075,6 +1079,8 @@ class HrpsysConfigurator(object):
         @param tm float: Time to complete.
         @param wait bool: If true, all other subsequent commands wait until
                           the movement commanded by this method call finishes.
+        @rtype bool
+        @return False upon any problem during execution.
         '''
         angles = [x / 180.0 * math.pi for x in pose]
         ret = self.seq_svc.setJointAnglesOfGroup(gname, angles, tm)
@@ -1093,6 +1099,8 @@ class HrpsysConfigurator(object):
         \endverbatim
         @param sequential list of angles in float: In rad
         @param tm sequential list of time in float: Time to complete, In Second
+        @rtype bool
+        @return False upon any problem during execution.
         '''
         for angles in angless:
             for i in range(len(angles)):
@@ -1111,6 +1119,8 @@ class HrpsysConfigurator(object):
         @param gname str: Name of the joint group.
         @param sequential list of angles in float: In rad
         @param tm sequential list of time in float: Time to complete, In Second
+        @rtype bool
+        @return False upon any problem during execution.
         '''
         for angles in angless:
             for i in range(len(angles)):
@@ -1185,7 +1195,7 @@ class HrpsysConfigurator(object):
 
     def getCurrentPose(self, lname=None, frame_name=None):
         '''!@brief
-        Returns the current physical pose of the specified joint.
+        Returns the current physical pose of the specified joint in the joint space.
         cf. getReferencePose that returns commanded value.
 
         eg.
@@ -1238,7 +1248,7 @@ class HrpsysConfigurator(object):
 
     def getCurrentPosition(self, lname=None, frame_name=None):
         '''!@brief
-        Returns the current physical position of the specified joint.
+        Returns the current physical position of the specified joint in Cartesian space.
         cf. getReferencePosition that returns commanded value.
 
         eg.
@@ -1306,7 +1316,7 @@ class HrpsysConfigurator(object):
 
     def getReferencePose(self, lname, frame_name=None):
         '''!@brief
-        Returns the current commanded pose of the specified joint.
+        Returns the current commanded pose of the specified joint in the joint space.
         cf. getCurrentPose that returns physical pose.
 
         @type lname: str
@@ -1338,7 +1348,7 @@ class HrpsysConfigurator(object):
 
     def getReferencePosition(self, lname, frame_name=None):
         '''!@brief
-        Returns the current commanded position of the specified joint.
+        Returns the current commanded position of the specified joint in Cartesian space.
         cf. getCurrentPosition that returns physical value.
 
         @type lname: str
@@ -1657,7 +1667,7 @@ dr=0, dp=0, dw=0, tm=10, wait=True):
         Get actual states of the robot that includes current reference joint angles and joint torques.
         @return: This returns actual states of the robot, which is defined in
         RobotHardware.idl
-        (https://github.com/fkanehiro/hrpsys-base/blob/master/idl/RobotHardwareService.idl#L33)
+        (found at https://github.com/fkanehiro/hrpsys-base/blob/3fd7671de5129101a4ade3f98e2eac39fd6b26c0/idl/RobotHardwareService.idl#L32_L57 as of version 315.11.0)
         \verbatim
             /**
              * @brief status of the robot
