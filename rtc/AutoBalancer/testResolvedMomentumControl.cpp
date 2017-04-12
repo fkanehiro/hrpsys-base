1cdcf97dbe632883b18b8da17b2da1d2d43731e2
Tatsuya Ishikawa <ishikawa@jsk.imi.i.u-tokyo.ac.jp>
2017-04-13 02:20:40 +0900

    [RMC] Add free joint test and plot constraint velocity

--- rtc/AutoBalancer/testResolvedMomentumControl.cpp
+++ rtc/AutoBalancer/testResolvedMomentumControl.cpp
@@ -53,17 +53,19 @@ private:
     {
         fprintf(gp, "%s\n unset multiplot\n", plot_str.c_str());
         fprintf(gp, "set terminal postscript eps color\nset output '/tmp/%s.eps'\n", graph_fname.c_str());
-        fprintf(gp, "%s\n unset multiplot\n", plot_str.c_str());
         fflush(gp);
     }
 
     void executeAndPlot(const double max_tm, calcReferenceParameter* calcRef)
     {
         std::string fname("/tmp/plot.dat");
-        // FILE* fp = fopen(fname.c_str(), "w");
         std::ofstream fp(fname.c_str());
         hrp::Vector3 Pref, Lref, cur_basePos, ref_basePos, P, L, CM;
         hrp::Matrix33 cur_baseRot, ref_baseRot;
+        hrp::dvector cur_q(m_robot->numJoints());
+        std::map<std::string, hrp::Vector3> p_constraint;
+        std::map<std::string, hrp::Matrix33> R_constraint;
+        std::map<std::string, hrp::dvector6> xi;
 
         for (double tm = 0; tm < max_tm; tm += dt) {
             Pref = calcRef->calcPref(m_robot, tm, max_tm);
@@ -72,32 +74,24 @@ private:
             for (std::map<std::string, hrp::dvector6>::iterator it = xi_ref.begin(); it != xi_ref.end(); ++it) {
                 (*it).second = calcRef->calcXiref(m_robot, (*it).first, tm, max_tm);
             }
-
-            hrp::dvector dq(m_robot->numJoints());
             for (size_t i = 0; i < m_robot->numJoints(); ++i) {
-                dq(i) = -m_robot->joint(i)->q;
+                cur_q(i) = m_robot->joint(i)->q;
             }
-
+            for (std::map<std::string, hrp::dvector6>::iterator it = xi_ref.begin(); it != xi_ref.end(); ++it) {
+                p_constraint[(*it).first] = m_robot->link((*it).first)->p;
+                R_constraint[(*it).first] = m_robot->link((*it).first)->R;
+            }
             cur_basePos = m_robot->rootLink()->p;
             cur_baseRot = m_robot->rootLink()->R;
-
             ref_basePos = cur_basePos + Pref / m_robot->totalMass() * dt;
             ref_baseRot = cur_baseRot;
 
+            // execute RMC
             rmc->rmControl(m_robot, Pref, Lref, xi_ref, ref_basePos, ref_baseRot, dt);
 
             m_robot->rootLink()->v = (m_robot->rootLink()->p - cur_basePos) / dt;
             hrp::Matrix33 dR = cur_baseRot.transpose() * m_robot->rootLink()->R;
             m_robot->rootLink()->w = hrp::omegaFromRot(dR) / dt;
-
-            // std::cerr << (m_robot->rootLink()->v).transpose() << std::endl;
-            // std::cerr << (m_robot->rootLink()->w).transpose() << std::endl;
-
-            for (size_t i = 0; i < m_robot->numJoints(); ++i) {
-                dq(i) += m_robot->joint(i)->q;
-                dq(i) /= dt;
-                m_robot->joint(i)->dq = dq(i);
-            }
 
             hrp::dvector tmpPL(6);
             tmpPL << Pref, Lref;
@@ -108,24 +102,14 @@ private:
             CM = m_robot->calcCM();
             m_robot->calcTotalMomentumFromJacobian(P, L);
 
-            // fprintf(fp, "%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f\n",
-            //         tm,
-            //         Pref(0),
-            //         P(0),
-            //         Pref(1),
-            //         P(1),
-            //         Pref(2),
-            //         P(2),
-            //         Lref(0),
-            //         L(0),
-            //         Lref(1),
-            //         L(1),
-            //         Lref(2),
-            //         L(2),
-            //         CM(0),
-            //         CM(1),
-            //         CM(2)
-            //         );
+            for (size_t i = 0; i < m_robot->numJoints(); ++i) {
+                m_robot->joint(i)->dq = (m_robot->joint(i)->q - cur_q(i)) / dt;
+            }
+            for (std::map<std::string, hrp::dvector6>::iterator it = xi_ref.begin(); it != xi_ref.end(); ++it) {
+                xi[(*it).first].segment(0, 3) = (m_robot->link((*it).first)->p - p_constraint[(*it).first]) / dt;
+                xi[(*it).first].segment(3, 3) = hrp::omegaFromRot(R_constraint[(*it).first].transpose() * m_robot->link((*it).first)->R) / dt;
+            }
+
             fp << std::fixed;
             fp << tm
                << " " << Pref(0)
@@ -140,19 +124,20 @@ private:
                << " " << L(1)
                << " " << Lref(2)
                << " " << L(2);
-            // for (size_t i = 0; i < dq.size(); ++i) {
-            //     fp << " " << dq(i);
+            // for (size_t i = 0; i < m_robot->numJoints(); ++i) {
+            //     fp << " " << m_robot->joint(i)->dq;
             // }
+            for (std::map<std::string, hrp::dvector6>::iterator it = xi_ref.begin(); it != xi_ref.end(); ++it) {
+                for (size_t i = 0; i < 6; ++i) {
+                    fp << " " << (*it).second(i)
+                       << " " << xi[(*it).first](i);
+                }
+            }
             fp << "\n";
         }
 
-        for (std::map<std::string, hrp::dvector6>::iterator it = xi_ref.begin(); it != xi_ref.end(); ++it) {
-            rmc->removeConstraintLink(m_robot, (*it).first, xi_ref);
-        }
-        delete calcRef;
-        // fclose(fp);
-
-        size_t gpsize = 2;
+        size_t gpsize = 2 + xi_ref.size() * 2; // P, L, xi_p, xi_R
+        size_t gp_count = 0;
         size_t start = 2;
         FILE* gps[gpsize];
         for (size_t ii = 0; ii < gpsize; ii++) {
@@ -166,13 +151,13 @@ private:
             std::string titles[3] = {"X", "Y", "Z"};
             for (size_t ii = 0; ii < 3; ii++) {
                 oss << "set xlabel 'Time [s]'" << std::endl;
-                oss << "set ylabel '" << titles[ii] << "[kg m/s]'" << std::endl;
+                oss << "set ylabel '" << titles[ii] << "[Ns]'" << std::endl;
                 oss << "plot "
-                    << "'" << fname << "' using 1:" << (start + ii * 2) << " with points ps 0.1 title 'Pref',"
-                    << "'" << fname << "' using 1:" << (start + ii * 2 + 1) << " with points ps 0.1 title 'P'"
+                    << "'" << fname << "' using 1:" << (start + ii * 2) << " with points ps 0.2 title 'Pref',"
+                    << "'" << fname << "' using 1:" << (start + ii * 2 + 1) << " with points ps 0.2 title 'P'"
                     << std::endl;
             }
-            plot_and_save(gps[0], gtitle, oss.str());
+            plot_and_save(gps[gp_count++], gtitle, oss.str());
             start += 6;
         }
         // L
@@ -185,11 +170,11 @@ private:
                 oss << "set xlabel 'Time [s]'" << std::endl;
                 oss << "set ylabel '" << titles[ii] << "[mNs]'" << std::endl;
                 oss << "plot "
-                    << "'" << fname << "' using 1:" << (start + ii * 2) << " with points ps 0.1 title 'Lref',"
-                    << "'" << fname << "' using 1:" << (start + ii * 2 + 1) << " with points ps 0.1 title 'L'"
+                    << "'" << fname << "' using 1:" << (start + ii * 2) << " with points ps 0.2 title 'Lref',"
+                    << "'" << fname << "' using 1:" << (start + ii * 2 + 1) << " with points ps 0.2 title 'L'"
                     << std::endl;
             }
-            plot_and_save(gps[1], gtitle, oss.str());
+            plot_and_save(gps[gp_count++], gtitle, oss.str());
             start += 6;
         }
         // // CoM
@@ -223,6 +208,47 @@ private:
         //     oss << std::endl;
         //     plot_and_save(gps[2], gtitle, oss.str());
         // }
+
+        // xi
+        for (std::map<std::string, hrp::dvector6>::iterator it = xi_ref.begin(); it != xi_ref.end(); ++it) {
+            // p
+            std::ostringstream oss("");
+            std::string gtitle("xi_p " + (*it).first);
+            oss << "set multiplot layout 3, 1 title '" << gtitle << "'" << std::endl;
+            std::string titles[3] = {"X", "Y", "Z"};
+            for (size_t ii = 0; ii < 3; ii++) {
+                oss << "set xlabel 'Time [s]'" << std::endl;
+                oss << "set ylabel '" << titles[ii] << "[m/s]'" << std::endl;
+                oss << "plot "
+                    << "'" << fname << "' using 1:" << (start + ii * 2) << " with points ps 0.2 title 'xi_p ref',"
+                    << "'" << fname << "' using 1:" << (start + ii * 2 + 1) << " with points ps 0.2 title 'xi_p'"
+                    << std::endl;
+            }
+            plot_and_save(gps[gp_count++], gtitle, oss.str());
+            start += 6;
+
+            // R
+            oss.str("");
+            oss.clear();
+            gtitle = "xi_R " + (*it).first;
+            oss << "set multiplot layout 3, 1 title '" << gtitle << "'" << std::endl;
+            // std::string titles[3] = {"X", "Y", "Z"};
+            for (size_t ii = 0; ii < 3; ii++) {
+                oss << "set xlabel 'Time [s]'" << std::endl;
+                oss << "set ylabel '" << titles[ii] << "[rad/s]'" << std::endl;
+                oss << "plot "
+                    << "'" << fname << "' using 1:" << (start + ii * 2) << " with points ps 0.2 title 'xi_R ref',"
+                    << "'" << fname << "' using 1:" << (start + ii * 2 + 1) << " with points ps 0.2 title 'xi_R'"
+                    << std::endl;
+            }
+            plot_and_save(gps[gp_count++], gtitle, oss.str());
+            start += 6;
+        }
+
+        for (std::map<std::string, hrp::dvector6>::iterator it = xi_ref.begin(); it != xi_ref.end(); ++it) {
+            rmc->removeConstraintLink(m_robot, (*it).first, xi_ref);
+        }
+        delete calcRef;
 
         double tmp;
         std::cin >> tmp;
@@ -290,6 +316,7 @@ public:
                 Lref(2) = 0.2 * sin(tm * M_PI / max_tm * 4);
                 return Lref;
             }
+
             hrp::dvector6 calcXiref(const hrp::BodyPtr m_robot, const std::string &constraint, const double tm, const double max_tm)
             {
                 hrp::dvector6 xi_ref = hrp::dvector6::Zero();
@@ -304,7 +331,7 @@ public:
 
     void test1()
     {
-        std::cerr << "test1 : Control All Momentum with lleg and rarm constraint" << std::endl;
+        std::cerr << "test1 : Control all momentum with lleg and rarm constraint" << std::endl;
         setResetPose();
 
         double max_tm = 4.0;
@@ -339,6 +366,7 @@ public:
                 Lref(2) = 0.2 * cos(tm * M_PI / max_tm * 4);
                 return Lref;
             }
+
             hrp::dvector6 calcXiref(const hrp::BodyPtr m_robot, const std::string &constraint, const double tm, const double max_tm)
             {
                 hrp::dvector6 xi_ref = hrp::dvector6::Zero();
@@ -352,7 +380,7 @@ public:
 
     void test2()
     {
-        std::cerr << "test2 : Control All Momentum with arms constraints" << std::endl;
+        std::cerr << "test2 : Control all momentum with arms constraints" << std::endl;
         setResetPose();
 
         double max_tm = 4.0;
@@ -387,6 +415,7 @@ public:
                 Lref(2) = 0.2 * cos(tm * M_PI / max_tm * 4);
                 return Lref;
             }
+
             hrp::dvector6 calcXiref(const hrp::BodyPtr m_robot, const std::string &constraint, const double tm, const double max_tm)
             {
                 hrp::dvector6 xi_ref = hrp::dvector6::Zero();
@@ -395,6 +424,55 @@ public:
         };
 
         calcRefParamTest2* calcRef = new calcRefParamTest2();
+        executeAndPlot(max_tm, calcRef);
+    }
+
+    void test3()
+    {
+        std::cerr << "test3 : Control pitch and yaw axis angular momentum with leg constraints" << std::endl;
+        setResetPose();
+
+        double max_tm = 4.0;
+        hrp::dvector6 Svec;
+        Svec << 0, 0, 0, 1, 0, 1;
+        rmc->setSelectionMatrix(Svec);
+        xi_ref[end_effectors["rleg"]] = hrp::dvector6::Zero(6, 1);
+        xi_ref[end_effectors["lleg"]] = hrp::dvector6::Zero(6, 1);
+
+        for (std::map<std::string, hrp::dvector6>::iterator it = xi_ref.begin(); it != xi_ref.end(); ++it) {
+            rmc->addConstraintLink(m_robot, (*it).first);
+        }
+
+        class calcRefParamTest3 : public calcReferenceParameter
+        {
+        public:
+            hrp::Vector3 calcPref(const hrp::BodyPtr m_robot, const double tm, const double max_tm)
+            {
+                hrp::Vector3 Pref;
+                Pref(0) = 0.01 * cos(tm * M_PI / max_tm * 4);
+                Pref(1) = 0.01 * sin(tm * M_PI / max_tm * 4);
+                Pref(2) = 0.02 * cos(tm * M_PI / max_tm * 4);
+                Pref *= m_robot->totalMass();
+                return Pref;
+            }
+
+            hrp::Vector3 calcLref(const hrp::BodyPtr m_robot, const double tm, const double max_tm)
+            {
+                hrp::Vector3 Lref;
+                Lref(0) = 0.1 * cos(tm * M_PI / max_tm * 4);
+                Lref(1) = 0.1 * sin(tm * M_PI / max_tm * 4);
+                Lref(2) = 0.2 * cos(tm * M_PI / max_tm * 4);
+                return Lref;
+            }
+
+            hrp::dvector6 calcXiref(const hrp::BodyPtr m_robot, const std::string &constraint, const double tm, const double max_tm)
+            {
+                hrp::dvector6 xi_ref = hrp::dvector6::Zero();
+                return xi_ref;
+            }
+        };
+
+        calcRefParamTest3* calcRef = new calcRefParamTest3();
         executeAndPlot(max_tm, calcRef);
     }
 
@@ -426,7 +504,7 @@ protected:
                                       17.835600, 9.137590, 6.611880, -36.456000, 0.000000, 0.000000, 0.000000,
                                       0.000000, 0.000000, 0.000000};
         for (size_t i = 0; i < m_robot->numJoints(); ++i) {
-            m_robot->joint(i)->q += deg2rad(reset_pose_angles[i]);
+            m_robot->joint(i)->q = deg2rad(reset_pose_angles[i]);
         }
         m_robot->calcForwardKinematics();
     }
@@ -456,6 +534,7 @@ void print_usage ()
     std::cerr << "  --test0 : Control All Momentum with legs constraint" << std::endl;
     std::cerr << "  --test1 : Control All Momentum with lleg and rarm constraint" << std::endl;
     std::cerr << "  --test2 : Control All Momentum with arms constraints" << std::endl;
+    std::cerr << "  --test3 : Control pitch and yaw axis angular momentum with leg constraints" << std::endl;
 }
 
 int main(int argc, char* argv[])
@@ -472,6 +551,8 @@ int main(int argc, char* argv[])
           trmc.test1();
       } else if (std::string(argv[1]) == "--test2") {
           trmc.test2();
+      } else if (std::string(argv[1]) == "--test3") {
+          trmc.test3();
       } else {
           print_usage();
           ret = 1;
