829ef3cbd05cc64b8052187090dc30786f1f94ce
Shunichi Nozawa <notheworld@gmail.com>
2017-05-22 00:15:25 +0900

    [rtc/AutoBalancer/testGaitGenerator.cpp] Add argument for default-double-support-ratio-swing-[after/before]

--- rtc/AutoBalancer/testGaitGenerator.cpp
+++ rtc/AutoBalancer/testGaitGenerator.cpp
@@ -818,6 +818,10 @@ public:
               if (++i < arg_strs.size()) gg->set_default_double_support_static_ratio_before(atof(arg_strs[i].c_str()));
           } else if ( arg_strs[i]== "--default-double-support-static-ratio-after" ) {
               if (++i < arg_strs.size()) gg->set_default_double_support_static_ratio_after(atof(arg_strs[i].c_str()));
+          } else if ( arg_strs[i]== "--default-double-support-ratio-swing-before" ) {
+              if (++i < arg_strs.size()) gg->set_default_double_support_ratio_swing_before(atof(arg_strs[i].c_str()));
+          } else if ( arg_strs[i]== "--default-double-support-ratio-swing-after" ) {
+              if (++i < arg_strs.size()) gg->set_default_double_support_ratio_swing_after(atof(arg_strs[i].c_str()));
           } else if ( arg_strs[i]== "--swing-trajectory-delay-time-offset" ) {
               if (++i < arg_strs.size()) gg->set_swing_trajectory_delay_time_offset(atof(arg_strs[i].c_str()));
           } else if ( arg_strs[i]== "--swing-trajectory-final-distance-weight" ) {
