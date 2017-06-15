dcfe52c8f2b995d2ba5ee16ecbcafdc40df4a8b9
yswhynot <yswhynot@gmail.com>
2017-06-15 11:46:26 -0400

    add collision loop service

--- rtc/CollisionDetector/CollisionDetector.cpp
+++ rtc/CollisionDetector/CollisionDetector.cpp
@@ -699,6 +699,13 @@ bool CollisionDetector::setTolerance(const char *i_link_pair_name, double i_tole
     return true;
 }
 
+bool CollisionDetector::setCollisionLoop(int input_loop) {
+    if ((input_loop > 0) && (input_loop < 999)) {
+        m_collision_loop = input_loop;
+    }
+    return false;
+}
+
 bool CollisionDetector::getCollisionStatus(OpenHRP::CollisionDetectorService::CollisionState &state)
 {
     state = m_state;
