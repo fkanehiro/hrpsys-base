e95d699b1fecd965b8f0043b857d7b9afcd5dd4c
Fumio KANEHIRO <fkanehiro@gmail.com>
2017-08-03 10:00:08 +0900

    enable to fix a link while playing patterns

--- rtc/SequencePlayer/SequencePlayer.h
+++ rtc/SequencePlayer/SequencePlayer.h
@@ -191,6 +191,9 @@ class SequencePlayer
   coil::Mutex m_mutex;
   double m_error_pos, m_error_rot;
   short m_iteration;
+  std::string m_fixedLink;
+  hrp::Vector3 m_fixedP;
+  hrp::Matrix33 m_fixedR;
   int dummy;
 };
 
