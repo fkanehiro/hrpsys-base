ce0100dc54213ed8964b5b7faf8da449f41b2d2c
Mirai Hattori <m-hattori@jsk.t.u-tokyo.ac.jp>
2019-02-20 16:03:52 +0900

    fix calcInverseKinematics2Loop with localPos and localR

--- rtc/ImpedanceController/JointPathEx.h
+++ rtc/ImpedanceController/JointPathEx.h
@@ -17,7 +17,8 @@ namespace hrp {
   public:
     JointPathEx(BodyPtr& robot, Link* base, Link* end, double control_cycle, bool _use_inside_joint_weight_retrieval = true, const std::string& _debug_print_prefix = "");
     bool calcJacobianInverseNullspace(dmatrix &J, dmatrix &Jinv, dmatrix &Jnull);
-    bool calcInverseKinematics2Loop(const Vector3& dp, const Vector3& omega, const double LAMBDA, const double avoid_gain = 0.0, const double reference_gain = 0.0, const dvector* reference_q = NULL);
+    bool calcInverseKinematics2Loop(const Vector3& dp, const Vector3& omega, const double LAMBDA, const double avoid_gain = 0.0, const double reference_gain = 0.0, const dvector* reference_q = NULL,
+                                    const hrp::Vector3& localPos = hrp::Vector3::Zero());
     bool calcInverseKinematics2Loop(const Vector3& end_effector_p, const Matrix33& end_effector_R,
                                     const double LAMBDA, const double avoid_gain = 0.0, const double reference_gain = 0.0, const hrp::dvector* reference_q = NULL,
                                     const double vel_gain = 1.0,
